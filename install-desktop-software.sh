9311c27f3136f1e3698b0625450b337dfc7ffada
Michael DeGuzis <mdeguzis@gmail.com>
2015-03-29 18:42:52 -0700

    added install/uninstall and list options

--- install-desktop-software.sh
+++ install-desktop-software.sh
@@ -4,7 +4,7 @@
 # Author: 	Michael DeGuzis
 # Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	install-desktop-software.sh
-# Script Ver:	0.1.2
+# Script Ver:	0.2.3
 # Description:	Adds various desktop software to the system for a more
 #		usable experience. Although this is not the main
 #		intention of SteamOS, for some users, this will provide
@@ -17,7 +17,7 @@
 # ------------------------------------------------------------------------
 
 # Set vars
-response="$1"
+type="$1"
 options="$2"
 apt_mode="install"
 uninstall="no"
@@ -57,7 +57,7 @@ if [[ "$1" == "--help" ]]; then
 	exit 0
 fi
 
-basic_software()
+install_software()
 {
 
 	# Set mode and proceed based on main() choice
@@ -71,45 +71,86 @@ basic_software()
 	# None here for now
 
 	# Wheezy-only software
-	sudo apt-get -t wheezy $apt_mode \
-	gparted \
-	baobab
+	sudo apt-get -t wheezy $apt_mode `cat software.temp`
 
+	# remove temp file
+	rm -f software.temp
+}
+
+get_software()
+{
+
+	# remove any exiting file
+	rm -f software.temp
+
+	# create temp file
+	touch software.temp
+
+	# Create listing based on $type
+        if [[ "$type" == "basic" ]]; then
+                # add basic software to temp list
+		cat > software.temp <<- EOF
+		gparted
+		baobab
+		EOF
+        elif [[ "$type" == "full" ]]; then
+                # add full softare to temp list
+		cat > software.temp <<- EOF
+		libreoffice
+		EOF
+        fi
+}
+
+show_warning()
+{
+        clear
+        printf "\nIn order to run this script, you MUST have had enabled the Debian\n"
+        printf "repositories! If you wish to exit, please press CTRL+C now..."
+        printf "\n\n type './install-debian-software --help' for assistance.\n"
+
+        read -n 1
+        printf "Continuing...\n"
+        sleep 1s
 }
 
 main()
 {
 
-	# Set default vars
-	type="basic"
+        # generate software listing based on type
+        get_software
 
-	clear
-	printf "\nIn order to run this script, you MUST have had enabled the Debian\n"
-	printf "repositories! If you wish to exit, please press CTRL+C now..."
-	printf "\n\n type './install-debian-software --help' for assistance.\n"
+	if [[ "$type" == "basic" ]]; then
 
-	read -n 1
-	printf "Continuing...\n"
-	sleep 1s
-
-	if [[ "$response" == "basic" ]]; then
-		basic_software
 		if [[ "$options" == "uninstall" ]]; then
         		uninstall="yes"
-		else
-                	# Nothing to see here for now
-                	echo /dev/null
+
+                elif [[ "$options" == "list" ]]; then
+                        # show listing from software.temp
+                        clear
+                        cat software.temp | less
+			rm -f software.temp
+			exit
 		fi
-	elif [[ "$reponse" == "full" ]]; then
-    		full_software
+
+		show_warning
+		install_software
+
+	elif [[ "$type" == "full" ]]; then
+
 		if [[ "$options" == "uninstall" ]]; then
                         uninstall="yes"
-                else
-                        # Nothing to see here for now
-                        echo /dev/null
-                fi
-	fi
 
+                elif [[ "$options" == "list" ]]; then
+                        # show listing from software.temp
+                        clear
+			cat software.temp | less
+			rm -f software.temp
+			exit
+                fi
+
+		show_warning
+		install_software
+	fi
 }
 
 # Start main function

e89bbeb445e7f9ad2ac05d75445ae2e9c2d2b1e0
mikeyd <mdeguzis@gmail.com>
2015-04-03 11:04:14 -0400

    testing EOF errors

--- add-debian-repos.sh
+++ add-debian-repos.sh
@@ -70,17 +70,17 @@ main()
 		fi
 	
 		# Create and add required text to preferences file
-cat <<- EOF >> ${prefer}
+	cat <<- EOF >> ${prefer}
 		Package: *
 		Pin: release l=Debian
 		Pin-Priority: 110
-EOF
+	EOF
 		
-cat <<- EOF >> ${steamosprefer}
+	cat <<- EOF >> ${steamosprefer}
 		Package: *
 		Pin: release l=SteamOS
 		Pin-Priority: 900
-EOF
+	EOF
 	
 		# Check for Wheezy list in repos.d
 		
@@ -93,11 +93,11 @@ EOF
 	
 		# Create and add required text to wheezy.list
 
-cat <<- EOF >> ${sourcelist}
+	cat <<- EOF >> ${sourcelist}
 		# Debian repo
 		deb ftp://mirror.nl.leaseweb.net/debian/ wheezy main contrib non-free
 		deb-src ftp://mirror.nl.leaseweb.net/debian/ wheezy main contrib non-free
-EOF
+	EOF
 
 		
 		# Update system

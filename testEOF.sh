0fbe56e77692fcc0a5926b59fd8445648cf89ff4
mikeyd <mdeguzis@gmail.com>
2015-04-03 14:21:47 -0400

    Update testEOF.sh

--- testEOF.sh
+++ testEOF.sh
@@ -1,8 +1,23 @@
 #! /bin/bash
-	
+
+{	
   prefer=/tmp/file
 			cat <<- EOF >> ${prefer}
 			Package: *
 			Pin: release l=Debian
 			Pin-Priority: 110
 			EOF
+			
+# test based on my code
+
+	# Install/Uninstall process
+	
+		# Create and add required text to preferences file
+		cat <<-EOF >> ${prefer}
+		Package: *
+		Pin: release l=Debian
+		Pin-Priority: 110
+		EOF
+
+}
+main

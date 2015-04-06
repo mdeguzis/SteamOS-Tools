0642ec4d1bf43ecfe4d4ee88652f75145faad5fb
mikeyd <mdeguzis@gmail.com>
2015-04-06 15:57:15 -0400

    formatting

--- install-desktop-software.sh
+++ install-desktop-software.sh
@@ -26,40 +26,39 @@ uninstall="no"
 
 show_help()
 {
-
-clear
-cat << EOF
-Warning: usage of this script is at your own risk!
-You have two options with this script:
-
-Basic
----------------------------------------------------------------
-Standard Debian desktop application loadout.
-Based on: http://distrowatch.com/table.php?distribution=debian
-
-Extra
----------------------------------------------------------------
-Extra software
-Based on feeback and personal preference.
-
-<pkg_name> 
----------------------------------------------------------------
-Any package you wish to specify yourself. Alchemist repos will be
-used first, followed by Debian Wheezy.
-
-For a complete list, type:
-'./install-debian-software list [basic|extra]'
-
-Install with:
-'./install-debian-software [install|uninstall|list] [basic|extra|<pkg_name>]'
-
-Press enter to continue...
-
-EOF
-
-read -n 1
-printf "Continuing...\n"
-clear
+	
+	clear
+	cat <<-EOF
+	Warning: usage of this script is at your own risk!
+	You have two options with this script:
+	
+	Basic
+	---------------------------------------------------------------
+	Standard Debian desktop application loadout.
+	Based on: http://distrowatch.com/table.php?distribution=debian
+	
+	Extra
+	---------------------------------------------------------------
+	Extra software
+	Based on feeback and personal preference.
+	
+	<pkg_name> 
+	---------------------------------------------------------------
+	Any package you wish to specify yourself. Alchemist repos will be
+	used first, followed by Debian Wheezy.
+	
+	For a complete list, type:
+	'./install-debian-software list [basic|extra]'
+	
+	Install with:
+	'./install-debian-software [install|uninstall|list] [basic|extra|<pkg_name>]'
+	
+	Press enter to continue...
+	EOF
+	
+	read -n 1
+	printf "Continuing...\n"
+	clear
 
 }
 

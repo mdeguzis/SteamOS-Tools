9e71508328746cd3bbcd1fd1a4985486616a4fd3
mikeyd <mdeguzis@gmail.com>
2015-04-07 06:34:02 -0400

    Update emu-from-source.sh

--- scriptmodules/emu-from-source.sh
+++ scriptmodules/emu-from-source.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # -------------------------------------------------------------------------------
-# Author:     	Michael DeGuzis
-# Git:		      https://github.com/ProfessorKaos64/SteamOS-Tools
-# Scipt Name:	  emu-from-source.sh
-# Script Ver:	  0.1.1
-# Description:	script to install emulation softare as a 
-#               part of install-debian-software.sh
-# Usage:	      N/A, called from another script
+# Author:     	  Michael DeGuzis
+# Git:		        https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	    emu-from-source.sh
+# Script Ver:	    0.1.1
+# Description:	  script to install emulation softare as a 
+#                 part of install-debian-software.sh
+# Usage:	        N/A, called from another script
 # -------------------------------------------------------------------------------
 
 efs_build_ra()

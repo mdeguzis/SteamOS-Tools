2339769fed752b8a67c3334e46b615676db572ab
mikeyd <mdeguzis@gmail.com>
2015-04-07 06:29:15 -0400

    Update emu-from-source.sh

--- scriptmodules/emu-from-source.sh
+++ scriptmodules/emu-from-source.sh
@@ -10,13 +10,39 @@
 # Usage:	      N/A, called from another script
 # -------------------------------------------------------------------------------
 
-efs_main()
+efs_build_ra()
+{
+  
+  mkdir -p ~/retroarch/cores
+  cd retroarch
+  make DESTDIR=~/retroarch install
+  cd ..
+  ./libretro-install.sh ~/retroarch/cores
+  
+}
+
+efs_build_cores()
 {
   
   echo "" > /dev/null
   
 }
 
+efs_main()
+{
+  
+  # clone and fetch libretro
+  git clone git://github.com/libretro/libretro-super.git
+  cd libretro-super
+  ./libretro-fetch.sh
+  
+  # build
+  efs_build_ra
+  
+  # clone and fetch super build (evaluating currently)
+  #git clone https://github.com/libretro/libretro-super
+  
+}
 
 # start main
 efs_main

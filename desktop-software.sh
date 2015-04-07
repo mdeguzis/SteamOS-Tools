0b478091f4eab71c82bc0c84d7600cc8437d389f
mikeyd <mdeguzis@gmail.com>
2015-04-07 06:46:40 -0400

    Update desktop-software.sh

--- desktop-software.sh
+++ desktop-software.sh
@@ -27,12 +27,64 @@ funct_vars()
 	uninstall="no"
 }
 
-funct_import_modules()
+funct_import_modules() 
 {
-	
-	# pull in modules for script
-	source "scriptmodules/emu-from-source.sh"
-	
+    
+    # @description importer routine to get external functionality.
+    # @description the first location searched is the script directory.
+    # @description if not found, search the module in the paths contained in $SHELL_LIBRARY_PATH environment variable
+    # @param $1 the .shinc file to import, without .shinc extension
+    module=$1
+
+    if [ -f $module.shinc ]; then
+      source $module.shinc
+      echo "Loaded module $(basename $module.shinc)"
+      return
+    fi
+
+    if test "x$module" == "x"
+    then
+	echo "$script_name : Unable to import unspecified module. Dying."
+        exit 1
+    fi
+
+	if test "x${script_absolute_dir:-notset}" == "xnotset"
+    then
+	echo "$script_name : Undefined script absolute dir. Did you remove getScriptAbsoluteDir? Dying."
+        exit 1
+    fi
+
+	if test "x$script_absolute_dir" == "x"
+    then
+	echo "$script_name : empty script path. Dying."
+        exit 1
+    fi
+
+    if test -e "$script_absolute_dir/$module.shinc"
+    then
+        # import from script directory
+        . "$script_absolute_dir/$module.shinc"
+        echo "Loaded module $script_absolute_dir/$module.shinc"
+        return
+    elif test "x${SHELL_LIBRARY_PATH:-notset}" != "xnotset"
+    then
+        # import from the shell script library path
+        # save the separator and use the ':' instead
+        local saved_IFS="$IFS"
+        IFS=':'
+        for path in $SHELL_LIBRARY_PATH
+        do
+          if test -e "$path/$module.shinc"
+          then
+                . "$path/$module.shinc"
+                return
+          fi
+        done
+        # restore the standard separator
+        IFS="$saved_IFS"
+    fi
+    echo "$script_name : Unable to find module $module"
+    exit 1
 }
 
 show_help()
@@ -217,6 +269,13 @@ show_warning()
 
 main()
 {
+	clear
+	
+	# load script modules
+	echo "#####################################################"
+	echo "Loading script modules"
+	echo "#####################################################"
+	import "scriptmodules/emu-from-source.sh"
 
         # generate software listing based on type
         get_software_type

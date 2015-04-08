4906ad43a0251bfefef6d0f1b57164da9ea56f22
mikeyd <mdeguzis@gmail.com>
2015-04-08 09:44:56 -0400

    testing skipping broken pkgs

--- desktop-software.sh
+++ desktop-software.sh
@@ -331,7 +331,7 @@ install_software()
 	for i in `cat $software_list`; do
 		# skip any pkgs marked broken (testing branch only)
 		# Install all others
-		if [ $i =~ "broken" ]; then
+		if [[ $i =~ "broken" ]]; then
 			echo -e "\nskipping broken package $i ...\n"
 			sleep 1s
 			# temp exit to test

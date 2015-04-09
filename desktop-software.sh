1f09c7ea47e7608b88a9d03677b898dc0a3d40db
mikeyd <mdeguzis@gmail.com>
2015-04-09 11:19:49 -0400

    testing trap errors

--- desktop-software.sh
+++ desktop-software.sh
@@ -395,12 +395,12 @@ install_software()
 				sudo apt-get $cache_tmp $apt_mode $i
 				
 				# return to loop if user hit "n" to removal instead of pushing onward
-				if [[ $? == 1 && "$apt_mode" == "remove" ]]; then
-					#test only
-					clear
-					echo "n caught, exiting"
-					exit
-				fi
+			#	if [[ $? == 1 && "$apt_mode" == "remove" ]]; then
+			#		#test only
+			#		clear
+			#		echo "n caught, exiting"
+			#		exit
+			#	fi
 			 
 				###########################################################
 				# Installation routine (wheezy - 2nd stage)

a34a9ce70d50e76d29cd15272c5374f0d6cbc7c6
mikeyd <mdeguzis@gmail.com>
2015-04-09 10:57:21 -0400

    testing custom pkg check

--- desktop-software.sh
+++ desktop-software.sh
@@ -735,12 +735,13 @@ main()
                 elif [[ "$options" == "check" ]]; then
 			# loop over packages and check
 			
+			clear
 			for i in `cat $software_list`; do
 				echo $i
 				PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $i | grep "install ok installed")
 				if [ "" == "$PKG_OK" ]; then
 					# dpkg outputs it's own line that can't be supressed
-					echo -e "Packge $i [Not Found]" > /dev/null
+					echo -e "Packge $i [Not Found]"
 				else
 					echo -e "Packge $i [OK]"
 					sleep 0.2s

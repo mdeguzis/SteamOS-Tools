a422779779a2ff2867267ec10a49b663b26ace3f
mikeyd <mdeguzis@gmail.com>
2015-04-10 13:48:53 -0400

    added missing gpg key for alchemist

--- build-test-chroot.sh
+++ build-test-chroot.sh
@@ -14,70 +14,94 @@
 #
 # -------------------------------------------------------------------------------
 
-# Install the required packages 
-apt-get install binutils debootstrap
-
-	# Warn user script must be run as root
-	if [ "$(id -u)" -ne 0 ]; then
-		clear
-		printf "\nScript must be run as root! Try:\n\n"
-		printf "'sudo $0 install'\n\n"
-		printf "OR\n"
-		printf "\n'sudo $0 uninstall'\n\n"
-		exit 1
-	fi
-
-if [[ "$1" == "-type" ]]; then
-  if [[ "$2" == "wheezy" ]]; then
-  
-    # create our chroot folder
-    if [[ -d "/home/desktop/wheezy-chroot" ]]; then
-    
-    # remove DIR
-    rm -rf "/home/desktop/wheezy-chroot"
-    fi
-    
-    # create DIR
-    mkdir -p "/home/desktop/wheezy-chroot"
-    
-    # buildin the environment
-    debootstrap --arch i386 wheezy /home/desktop/wheezy-chroot http://http.debian.net/debian
-    
-    # enter chroot to test
-    chroot /home/desktop/wheezy-chroot
-    
-    # kick back if failure
-    if [ $? == '0' ]; then
-      echo -e "\nFailed to enter chroot. Please try again\n"
-    fi
-    
-    # create dpkg policy for daemons
-    chroot /srv/chroot/wheezy
-    cat > ./usr/sbin/policy-rc.d <<-EOF
-    #!/bin/sh
-    exit 101
-    EOF
-    chmod a+x ./usr/sbin/policy-rc.d
-    
-    # Several packages depend upon ischroot for determining correct 
-    # behavior in a chroot and will operate incorrectly during upgrades if it is not fixed.
-    dpkg-divert --divert /usr/bin/ischroot.debianutils --rename /usr/bin/ischroot
-    ln -s /bin/true /usr/bin/ischroot
-    
-    # "bind" /dev/pts
-    mount --bind /dev/pts /srv/chroot/wheezy/dev/pts
-    
-    # eliminate unecessary packages
-    apt-get install deborphan
-    deborphan -a
-    
-    # exit chroot
-    exit
-
-  elif [[ "$2" == "steamos" ]]; then
-    # nothing to see here for now
-    echo "" > /dev/null
-  fi
+# Warn user script must be run as root
+if [ "$(id -u)" -ne 0 ]; then
+	clear
+	printf "\nScript must be run as root! Try:\n\n"
+	printf "'sudo $0 install'\n\n"
+	printf "OR\n"
+	printf "\n'sudo $0 uninstall'\n\n"
+	exit 1
 fi
 
+funct_prereqs()
+{
+	
+	# Install the required packages 
+	apt-get install binutils debootstrap debian-archive-keyring
+	
+}
 
+funct_set_target()
+{
+
+	if [[ "$1" == "-type" ]]; then
+	  if [[ "$2" == "wheezy" ]]; then
+	  	target="wheezy"
+	  	release="wheezy"
+	  	target_URL="http://http.debian.net/debian"
+	  elif [[ "$2" == "steamos" ]]; then
+		target="steamos"
+		release="alchemist"
+		target_URL="http://http.repo.steampowered.com/steamos"
+	    
+	  fi
+	fi
+	
+}
+
+fucnt_create_chroot()
+{
+	
+	#import GPG key for SteamOS alchemist
+	gpg --no-default-keyring --keyring /usr/share/keyrings/debian-archive-keyring.gpg --recv-keys 7DEEB7438ADDD96
+	
+	# create our chroot folder
+	if [[ -d "/home/desktop/${target}-chroot" ]]; then
+		# remove DIR
+		rm -rf "/home/desktop/${target}-chroot"
+	fi
+	
+	# create DIR
+	mkdir -p "/home/desktop/${target}-chroot"
+	
+	# buildin the environment
+	/usr/sbin/debootstrap --arch i386 ${release} /home/desktop/${target}-chroot ${target_URL}
+	
+	# enter chroot to test
+	chroot /home/desktop/${target}-chroot
+	
+	# kick back if failure
+	if [ $? == '0' ]; then
+	echo -e "\nFailed to enter chroot. Please try again\n"
+	fi
+	
+	# create dpkg policy for daemons
+	chroot /srv/chroot/${target}
+	cat > ./usr/sbin/policy-rc.d <<-EOF
+	#!/bin/sh
+	exit 101
+	EOF
+	chmod a+x ./usr/sbin/policy-rc.d
+	
+	# Several packages depend upon ischroot for determining correct 
+	# behavior in a chroot and will operate incorrectly during upgrades if it is not fixed.
+	dpkg-divert --divert /usr/bin/ischroot.debianutils --rename /usr/bin/ischroot
+	ln -s /bin/true /usr/bin/ischroot
+	
+	# "bind" /dev/pts
+	mount --bind /dev/pts /home/desktop/${target}-chroot/dev/pts
+	
+	# eliminate unecessary packages
+	apt-get install deborphan
+	deborphan -a
+	
+	# exit chroot
+	exit
+	
+}
+
+# Routines
+funct_prereqs
+funct_set_target
+fucnt_create_chroot

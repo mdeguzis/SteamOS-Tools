0b821bdba3b5e3a3359a34e3c4522508c71631ff
mikeyd <mdeguzis@gmail.com>
2015-04-10 15:55:33 -0400

    Update build-test-chroot.sh

--- build-test-chroot.sh
+++ build-test-chroot.sh
@@ -119,6 +119,10 @@ funct_create_chroot()
 	
 	/usr/sbin/chroot "/home/desktop/${target}-chroot"
 	
+	# The rest below is commented out. Likely a helper script can 
+	# be made to kick off the config with in the chroot.
+	#http://www.cyberciti.biz/faq/unix-linux-chroot-command-examples-usage-syntax/
+	
 	# pass to ensure we are in the chroot 
 	#if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
 	#	echo "We are chrooted!"
@@ -131,46 +135,46 @@ funct_create_chroot()
 	#fi
 	
 	# opt into beta in chroot if flag is thrown
-	if [[ "$beta_flag" == "yes" ]]; then
-		# add beta repo and update
-		apt-get install steamos-beta-repo -y
-		apt-get update -y
-		apt-get upgrade -y
-	elif [[ "$beta_flag" == "no" ]]; then
-		# do nothing
-		echo "" > /dev/null
-	fi
+	#if [[ "$beta_flag" == "yes" ]]; then
+	#	# add beta repo and update
+	#	apt-get install steamos-beta-repo -y
+	#	apt-get update -y
+	#	apt-get upgrade -y
+	#elif [[ "$beta_flag" == "no" ]]; then
+	#	# do nothing
+	#	echo "" > /dev/null
+	#fi
 	
 	# create dpkg policy for daemons
-	chroot /home/desktop/${target}
-	cat > ./usr/sbin/policy-rc.d <<-EOF
+	#chroot /home/desktop/${target}
+	#cat > ./usr/sbin/policy-rc.d <<-EOF
 	#!/bin/sh
-	exit 101
-	EOF
-	chmod a+x ./usr/sbin/policy-rc.d
+	#exit 101
+	#EOF
+	#chmod a+x ./usr/sbin/policy-rc.d
 	
 	# Several packages depend upon ischroot for determining correct 
 	# behavior in a chroot and will operate incorrectly during upgrades if it is not fixed.
-	dpkg-divert --divert /usr/bin/ischroot.debianutils --rename /usr/bin/ischroot
+	#dpkg-divert --divert /usr/bin/ischroot.debianutils --rename /usr/bin/ischroot
 	
-	if [[ -f "/usr/bin/ischroot" ]]; then
-		# remove link
-		/usr/bin/ischroot
-	else
-		ln -s /bin/true /usr/bin/ischroot
-	fi
+	#if [[ -f "/usr/bin/ischroot" ]]; then
+	#	# remove link
+	#	/usr/bin/ischroot
+	#else
+	#	ln -s /bin/true /usr/bin/ischroot
+	#fi
 	
 	# "bind" /dev/pts
-	mount --bind /dev/pts /home/desktop/${target}-chroot/dev/pts
+	#mount --bind /dev/pts /home/desktop/${target}-chroot/dev/pts
 	
 	# eliminate unecessary packages
-	apt-get -t wheezy install deborphan
-	deborphan -a
+	#apt-get -t wheezy install deborphan
+	#deborphan -a
 	
 	# exit chroot
-	echo -e "\nExiting chroot!\n"
-	sleep 2s
-	exit
+	#echo -e "\nExiting chroot!\n"
+	#sleep 2s
+	#exit
 	
 }
 

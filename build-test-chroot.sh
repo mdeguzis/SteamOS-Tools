415434ec6bd1efa64134a2f292a96b733c76fb48
mikeyd <mdeguzis@gmail.com>
2015-04-13 13:44:53 -0400

    Update build-test-chroot.sh

--- build-test-chroot.sh
+++ build-test-chroot.sh
@@ -158,8 +158,11 @@ instead...\n"
 
 test()
 {
+
+		# Capture input
+		read stock_choice
 	
-		if [[ "$stock_choice" == "" ]]; then
+	if [[ "$stock_choice" == "" ]]; then
 		# Captured carriage return / blank line only, continue on as normal
 		# Modify target based on opts
 		#sed -i "s|"stock_tmp"|"no"|g" "/home/desktop/test-chroot/tmp/chroot-post-install.sh"

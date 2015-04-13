d1d8906c28af456955f3ef82d32fe37785d74578
mikeyd <mdeguzis@gmail.com>
2015-04-13 13:51:27 -0400

    Update build-test-chroot.sh

--- build-test-chroot.sh
+++ build-test-chroot.sh
@@ -136,13 +136,12 @@ instead...\n"
 	if [[ "$stock_choice" == "" ]]; then
 		# Captured carriage return / blank line only, continue on as normal
 		# Modify target based on opts
-		#sed -i "s|"stock_tmp"|"no"|g" "/home/desktop/${target}-chroot/tmp/chroot-post-install.sh"
+		sed -i "s|"stock_tmp"|"no"|g" "/home/desktop/${target}-chroot/tmp/chroot-post-install.sh"
 		printf "zero length detected..."
 		
 	elif [[ "$stock_choice" == "stock" ]]; then
 		# Modify target based on opts
-		echo "stock detected"
-		#sed -i "s|"stock_tmp"|"yes"|g" "/home/desktop/${target}-chroot/tmp/chroot-post-install.sh"
+		sed -i "s|"stock_tmp"|"yes"|g" "/home/desktop/${target}-chroot/tmp/chroot-post-install.sh"
 		
 	elif [[ "$stock_choice" != "stock" ]]; then
 		# user entered something arbitrary, exit

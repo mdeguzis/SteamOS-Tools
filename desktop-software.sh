70fd7982a9351820d7ac7884cb1aa3afd704d95f
mikeyd <mdeguzis@gmail.com>
2015-04-14 14:35:43 -0400

    ver bump

--- desktop-software.sh
+++ desktop-software.sh
@@ -4,7 +4,7 @@
 # Author: 	Michael DeGuzis
 # Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	install-desktop-software.sh
-# Script Ver:	0.6.3
+# Script Ver:	0.7.1
 # Description:	Adds various desktop software to the system for a more
 #		usable experience. Although this is not the main
 #		intention of SteamOS, for some users, this will provide

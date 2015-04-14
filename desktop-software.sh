d7b100f5bd1874a64dd4dac39d2572950f919d86
mikeyd <mdeguzis@gmail.com>
2015-04-14 10:39:10 -0400

    testing software listing output errors

--- desktop-software.sh
+++ desktop-software.sh
@@ -275,6 +275,9 @@ get_software_type()
                 # add emulation softare to temp list
                 software_list="$scriptdir/cfgs/emulation-src-deps.txt"
 	fi
+	
+	echo $software_list
+	exit
         
 	####################################################
 	# popular software / custom specification

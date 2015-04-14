3c8c990cb9d69ead6d7e9ccc996693600a17de20
mikeyd <mdeguzis@gmail.com>
2015-04-14 08:49:41 -0400

    Update desktop-software.sh

--- desktop-software.sh
+++ desktop-software.sh
@@ -347,7 +347,7 @@ install_software()
         # or if we are just checking packages
         
 	if [[ "$options" != "uninstall" && "$options" != "check" ]]; then
-	        echo -e "\nUpdating system, please wait...\n"
+	        echo -e "Updating system, please wait...\n"
 		sleep 1s
 	        sudo apt-key update
 	        sudo apt-get update

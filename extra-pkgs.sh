d45b2ced332bd4925c21bce6501701759e65fd5a
mikeyd <mdeguzis@gmail.com>
2015-04-15 18:49:04 -0400

    Update and rename vaporos-pkgs.sh to extra-pkgs.sh

--- extra-pkgs.sh
+++ extra-pkgs.sh
@@ -2,9 +2,10 @@
 # -----------------------------------------------------------------------
 # Author: 		Michael DeGuzis
 # Git:		      	https://github.com/ProfessorKaos64/scripts
-# Scipt Name:	  	vaporos-pkgs.sh
+# Scipt Name:	  	extra-pkgs.
 # Script Ver:	  	0.1.3
-# Description:		Installs useful pacakges from VaporOS 2
+# Description:		Installs useful pacakges otherwise not found in
+#			The Debian Wheezy repositories directly.
 #	
 # Usage:	      	./vaporos-pkgs.sh
 # ------------------------------------------------------------------------
@@ -15,22 +16,24 @@ sudo rm -f "log_temp.txt"
 main()
 {
 
-	#####################################################"
+	#####################################################
 	# VaporOS bindings (controller shortcuts)
-	#####################################################"
+	#####################################################
 	# FPS + more binds from VaporOS 2
 	# For bindings, see: /etc/actkbd-steamos-controller.conf
 	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' vaporos-binds-xbox360 | grep "install ok installed")
 	if [ "" == "$PKG_OK" ]; then
 		echo -e "vaporos-binds-xbox360 not found. Setting up vaporos-binds-xbox360 now...\n"
-		sleep 1s
-		cd ~/Downloads
-		wget https://github.com/sharkwouter/steamos-installer/blob/master/pool/main/v/vaporos-binds-xbox360/vaporos-binds-xbox360_1.0_all.deb
-		sudo dpkg -i vaporos-binds-xbox360_1.0_all.deb
+		sleep 2s
+		wget -P /tmp "https://github.com/sharkwouter/steamos-installer/blob/master/pool/main/v/vaporos-binds-xbox360/vaporos-binds-xbox360_1.0_all.deb"
+		sudo dpkg -i "/tmp/vaporos-binds-xbox360_1.0_all.deb"
+		#cleanup
+		rm -f "/tmp/vaporos-binds-xbox360_1.0_all.deb"
+		
 		cd
 		if [ $? == '0' ]; then
 			echo "Successfully installed 'vaporos-binds-xbox360'"
-			sleep 3s
+			sleep 2s
 		else
 			echo "Could not install 'vaporos-binds-xbox360'. Exiting..."
 			sleep 3s
@@ -38,6 +41,34 @@ main()
 		fi
 	else
 		echo "Checking for 'vaporos-binds-xbox360 [OK]'."
+		sleep 0.5s
+	fi
+	
+	#####################################################
+	# Firefox (imported 20150415)
+	#####################################################
+	# Imported from the Linux Mint LMDE 2 repository
+	# Deb binary source: deb http://packages.linuxmint.com debian import
+	
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' firefox | grep "install ok installed")
+	if [ "" == "$PKG_OK" ]; then
+		echo -e "\nFirefox not found. Setting up firefox now...\n"
+		sleep 2s
+		wget -P /tmp "http://www.libregeek.org/SteamOS-Extra/browsers/firefox_37.0~linuxmint1+betsy_amd64.deb"
+		sudo dpkg -i "/tmp/firefox_37.0~linuxmint1+betsy_amd64.deb"
+		# cleanup
+		rm -f "/tmp/firefox_37.0~linuxmint1+betsy_amd64.deb"
+		
+		if [ $? == '0' ]; then
+			echo "Successfully installed 'Firefox'"
+			sleep 2s
+		else
+			echo "Could not install 'Firefox'. Exiting..."
+			sleep 3s
+			exit 1
+		fi
+	else
+		echo "Checking for 'Firefox [OK]'."
 		sleep 0.5s
 	fi
 
@@ -61,4 +92,3 @@ sed -i 's|\[J||g' log.txt
 
 # remove file not needed anymore
 sudo rm -f "log_temp.txt"
-

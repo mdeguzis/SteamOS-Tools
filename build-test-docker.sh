cfed27c107da0542266594c1ceaddddad8b6e933
mikeyd <mdeguzis@gmail.com>
2015-04-17 09:08:13 -0400

    Update build-test-docker.sh

--- build-test-docker.sh
+++ build-test-docker.sh
@@ -26,27 +26,27 @@ opt2="$2"
 
 show_help()
 {
-  
-  clear
-
+	
+	clear
+	
 	cat <<-EOF
 	#####################################################
 	Quick usage notes:
 	#####################################################
-  
-  Docker is now installed. 
-  To ask docker for a list of all available commands:
-
-  `sudo docker`
-  
-  For a quick demo of how it works, take a look here:
-  https://www.docker.com/tryit/
-
-  The user guide can be found here:
-  https://docs.docker.com/userguide/
-  
-  Enjoy!
-  
+	
+	Docker is now installed. 
+	To ask docker for a list of all available commands:
+	
+	`sudo docker`
+	
+	For a quick demo of how it works, take a look here:
+	https://www.docker.com/tryit/
+	
+	The user guide can be found here:
+	https://docs.docker.com/userguide/
+	
+	Enjoy!
+	
 	EOF
 	
 	echo ""
@@ -54,38 +54,41 @@ show_help()
 
 main()
 {
-  
-  # import verification keys
-  sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
-  
-  # Obtaining Docker
-  mkdir -p /home/desktop/dockter-testing
-  cd docker-testing/
-  curl -sSL https://get.docker.com/ | sed "s/|debian/|steamos|debian/g"|sh
-  
-  # add user to dockter group
-  sudo usermod -aG docker desktop
-  
-  # start the docker daemon if it hasn't been already
-  if [[ -f /var/run/docker.pid ]]; then
-    # start daemon
-    sudo docker -d &
-  fi
-  
-  # show quick help
-  show_help
-  
-  # confirm docker is installed
-  install_check=$(which docker)
-  
-  if [[ "$install_check" == "/usr/bin/docker" ]]; then
-    echo -e "\n Docker successfully installed!\n"
-    sleep 2s
-  else
-    echo -e "\n Docker failed to install!\n"
-    exit
-    sleep 2s
-  fi
+	clear
+	echo -e "\n==> import verification keys\n"
+	sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+	
+	echo -e "\n==>Obtaining Docker\n"
+	mkdir -p /home/desktop/dockter-testing
+	cd docker-testing/
+	curl -sSL https://get.docker.com/ | sed "s/|debian/|steamos|debian/g"|sh
+
+	echo -e "\n==> Post install commands\n"
+	# add user to dockter group
+	sudo usermod -aG docker desktop
+	
+	# start the docker daemon if it hasn't been already
+	if [[ -f /var/run/docker.pid ]]; then
+		# start daemon
+		sudo docker -d &
+	fi
+	
+	# show quick help
+	show_help
+	
+	echo -e "\n==> Installation check\n"
+	
+	# confirm docker is installed
+	install_check=$(which docker)
+	
+	if [[ "$install_check" == "/usr/bin/docker" ]]; then
+		echo -e "\n Docker successfully installed!\n"
+		sleep 2s
+	else
+		echo -e "\n Docker failed to install!\n"
+		exit
+		sleep 2s
+	fi
   
 }
 

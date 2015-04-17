a4ce01adb5a181f25b359bf89a682aba12f11239
mikeyd <mdeguzis@gmail.com>
2015-04-17 11:01:19 -0400

    Update build-test-docker.sh

--- build-test-docker.sh
+++ build-test-docker.sh
@@ -79,7 +79,7 @@ main()
 	# Create and add required text to preferences file
 	echo -e "\n==> Set /apt/preferences.d"
 	
-	sudo bash -c 'cat <<-EOF >> $prefer
+	sudo bash -s 'cat <<-EOF >> $prefer
 	deb http://get.docker.io/ubuntu docker main
 	EOF'
 	

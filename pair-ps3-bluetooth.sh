e2fe1cd28cf41d595e1e7b00a98f006619336de5
mikeyd <mdeguzis@gmail.com>
2015-04-20 19:20:08 -0400

    Update pair-ps3-bluetooth.sh

--- pair-ps3-bluetooth.sh
+++ pair-ps3-bluetooth.sh
@@ -3,31 +3,34 @@
 main()
 {
   
-  # Download qtsixad or sixpair:
-  # This is a rebuilt deb package from the ppa:falk-t-j/qtsixa PPA
-  wget -P /tmp "http://www.libregeek.org/SteamOS-Extra/<PS3 PKG LINK HERE>"
-
-  # Install and start sixad daemon.
-  sudo dpkg -i sixad_20131215-1_amd64.deb
-  sudo update-rc.d sixad defaults
-  sudo /etc/init.d/sixad enable
-  sudo /etc/init.d/sixad start
+	# Download qtsixad and sixad
+	# These are Debian rebuilt packages from the ppa:falk-t-j/qtsixa PPA
+	wget -P /tmp "http://www.libregeek.org/SteamOS-Extra/utilities/qtsixa_1.5.1+git20140130-SteamOS_amd64.deb"
+	wget -P /tmp "http://www.libregeek.org/SteamOS-Extra/utilities/sixad_1.5.1+git20130130-SteamOS_amd64.deb"
+	
+	# Install and start sixad daemon.
+	sudo dpkg -i "/tmp/qtsixa_1.5.1+git20140130-SteamOS_amd64.deb"
+	sudo dpkg -i "/tmp/sixad_1.5.1+git20130130-SteamOS_amd64.deb"
+	
+	sudo update-rc.d sixad defaults
+	sudo /etc/init.d/sixad enable
+	sudo /etc/init.d/sixad start
   
-  cmd=(dialog --backtitle "LibreGeek.org RetroRig Installer" \
+	cmd=(dialog --backtitle "LibreGeek.org RetroRig Installer" \
 		    --menu "Please select the number of PS3 controllers" 16 47 16)
 	options=(1 "1"
 	 	 2 "2"
 	 	 3 "3"
 	 	 4 "4")
 
-#make menu choice
+	#make menu choice
 	selection=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 	#functions
-
+	
 	for choice in $selection
 	do
 		case $choice in
-
+	
 		1)
 		
 		# call pairing function to set current bluetooth MAC to Player 1
@@ -35,7 +38,7 @@ main()
 		ps3_pair_blu
 		dialog --msgbox "Pairing of Player 1 Controller complete" 5 43 
 		;;
-
+	
 		2)
 		
 		# call pairing function to set current bluetooth MAC to Player 1
@@ -49,67 +52,67 @@ main()
 		ps3_pair_blu
 		dialog --msgbox "Pairing of Player 1 Controller complete" 5 43 
 		;;
-
+	
 		3)
-
+	
 		# call pairing function to set current bluetooth MAC to Player 1
 		n="1"
 		ps3_pair_blu
 		ps3_pair_blu
 		dialog --msgbox "Pairing of Player 1 Controller complete" 5 43 
- 
+	
 		# call pairing function to set current bluetooth MAC to Player 2
 		n="2"
 		ps3_pair_blu
 		ps3_pair_blu
 		dialog --msgbox "Pairing of Player 1 Controller complete" 5 43 
-
+	
 		# call pairing function to set current bluetooth MAC to Player 3
 		n="3"
 		ps3_pair_blu
 		dialog --msgbox "Pairing of Player 1 Controller complete" 5 43 
 		;;
-
+	
 		4)
 		# call pairing function to set current bluetooth MAC to Player 1
 		n="1"
 		ps3_pair_blu
 		dialog --msgbox "Pairing of Player 1 Controller complete" 5 43 
-
+	
 		esac
 		
 	done
-
+	
 	###########################################################
 	# End controller pairing process
 	###########################################################
-
+	
 	# start the service at boot time
 	sixad --boot-yes
-
+	
 }
-
+	
 ps3_pair_blu()
 {
-
+	
 	dialog --msgbox "Please plug in these items now:\n\n1)The USB cable\n2)PS3 controller $n \n
-3)Bluetooth dongle\n\nAdditional controllers can be added in the settings menu"  12 40
-
+	3)Bluetooth dongle\n\nAdditional controllers can be added in the settings menu"  12 40
+	
 	# Grab player 1 controller MAC Address of wired device
 	echo -e "\nSetting up Playstation 3 Sixaxis (bluetooth) [Player $n]"\n"
 	sleep 2s
-
+	
 	# Pair controller with logging
 	sudo sixpair
 	sleep 2s
-
+	
 	# Inform player 1 controller user to disconnect USB cord
 	dialog --msgbox "Please disconnect the USB cable and press the PS Button now. The appropriate \
-LED for player $n should be lit. If it is not, please hold in the PS button to turn it off, then back on.\n\nThere is no need to reboot to \
-fully enable the controller(s)" 12 60
-
-  echo -e "\nUsing the left stick and pressing the left and right stick navigate to the Settings Screen 
-  and edit the layout of the controller."
+	LED for player $n should be lit. If it is not, please hold in the PS button to turn it off, then back on.\n\nThere is no need to reboot to \
+	fully enable the controller(s)" 12 60
+	
+	echo -e "\nUsing the left stick and pressing the left and right stick navigate to the Settings Screen 
+	and edit the layout of the controller."
 
 }
 

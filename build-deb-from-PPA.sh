e0d2a55377cae47cd2aa28e15a48fc2b9fcd5b6e
ProfessorKaos64 <mdeguzis@gmail.com>
2015-04-22 18:12:39 -0400

    added deb builder script

---
+++ build-deb-from-PPA.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------
+# Author:    	Michael DeGuzis
+# Git:	    	https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	build-deb-from-PPA.sh
+# Script Ver:	0.1.1
+# Description:	Attempts to build a deb package from a PPA
+#
+# Usage:	sudo ./build-deb-from-PPA.sh
+#
+# -------------------------------------------------------------------------------
+
+install_prereqs()
+{
+
+	apt-get install devscripts build-essential
+
+}
+
+main()
+{
+
+	
+
+}
+
+# start main

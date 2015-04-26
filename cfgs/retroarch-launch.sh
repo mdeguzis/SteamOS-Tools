dd0a22e18d015e67e547be0d0c628ec24a621568
mikeyd <mdeguzis@gmail.com>
2015-04-26 14:04:22 -0400

    Update retroarch-launch.sh

--- cfgs/retroarch-launch.sh
+++ cfgs/retroarch-launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # launch retroarch based on current user so we use the right cfg file
-retroarch --config "$HOME/.config/retroarch/retroarch.cfg"
+retroarch --config "$HOME/.config/retroarch/retroarch.cfg" --menu

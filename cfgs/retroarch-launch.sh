63852c7d6f38f3d74cdea924960bf71febd16d70
mikeyd <mdeguzis@gmail.com>
2015-04-26 12:28:08 -0400

    set lauch params

--- cfgs/retroarch-launch.sh
+++ cfgs/retroarch-launch.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# these short commands will swap the configuration file basedon the current user
-current_cfg=$(grep "Exec=retroarch" $HOME/.config/retroarch/retroarch.cfg)
-new_cfg="retroarch --config $HOME/.config/retroarch/retroarch.cfg"
-
-# perform swap
-sudo sed -i 's|$current_cfgd|$new_cfg|g' "$HOME/.config/retroarch/retroarch.cfg"
+# launch retroarch based on current user so we use the right cfg file
+retroarch --config "$HOME/.config/retroarch/retroarch.cfg"

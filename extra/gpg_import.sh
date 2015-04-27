64960f8d00a5d0ef86b03c4b0ac85a2a91f9be2c
mikeyd <mdeguzis@gmail.com>
2015-04-27 15:05:41 -0400

    update key var gpg import script

--- extra/gpg_import.sh
+++ extra/gpg_import.sh
@@ -3,12 +3,12 @@
 # Author:     		Michael DeGuzis
 # Git:		      	https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	  	emu-from-source.sh
-# Script Ver:	  	0.1.1
+# Script Ver:	  	0.1.2
 # Description:		script to add and export a gpg key
 # -------------------------------------------------------------------------------
-arg="$1"
+key="$1"
 keyserver="hkp://subkeys.pgp.net"
 
 #--no-default-keyring --keyring /usr/share/keyrings/debian-archive-keyring.gpg
-gpg --keyserver  $keyserver --recv-keys $1
-gpg -a --export $1 | sudo apt-key add -
+gpg --keyserver  $keyserver --recv-keys $key
+gpg -a --export $key | sudo apt-key add -

e30994d1a8fea0d17255bc273d2f3c9095c83bf1
mikeyd <mdeguzis@gmail.com>
2015-04-28 08:20:09 -0400

    testing warning output update

--- desktop-software.sh
+++ desktop-software.sh
@@ -600,8 +600,8 @@ show_warning()
         	echo -e " On initial check, those sources appear to be added."
         fi
         	
-        echo -e "If you wish to exit, please press CTRL+C now..."
-        echo -e "\ntype './desktop-software --help' for assistance.\n"
+        echo -e "\nIf you wish to exit, please press CTRL+C now..."
+        echo -e "\ntype './desktop-software --help' (without quotes) for assistance.\n"
         echo -e "See log.txt in this direcotry after any attempt for details"
         echo -e "If you need to add the Debian repos, please use the"
         echo -e "desktop-software.sh script in the main repository folder..\n"

29013e06681a5b8c1aa5925a607841b644134541
mikeyd <mdeguzis@gmail.com>
2015-05-01 08:08:02 -0400

    Update gpg_import.sh

--- extra/gpg_import.sh
+++ extra/gpg_import.sh
@@ -18,12 +18,22 @@ key_short=$(echo $key | cut -c 8-16)
 # check key first to avoid importing twice
 gpg_key_check=$(gpg --list-keys "$key_short")
 
+# allow script to run in current DIR if not called previously
+
+if [[ "$scriptdir" != ]]; then
+  # set to scriptdir
+  gpg_cmd="$scriptdir/extra/gpg_import.sh $key"
+else
+  # run from current dir
+  gpg_cmd="gpg_import.sh"
+fi
+
 if [[ "$gpg_key_check" != "" ]]; then
   echo -e "\nGPG key "$key" [OK]\n"
   sleep 1s
 else
   echo -e "\nGPG key "$key" [FAIL]. Adding now...\n"
-  $scriptdir/extra/gpg_import.sh $key
+  "$gpg_cmd"
 fi
 
 #--no-default-keyring --keyring /usr/share/keyrings/debian-archive-keyring.gpg

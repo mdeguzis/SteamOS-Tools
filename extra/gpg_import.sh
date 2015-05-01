c2b1bae0940e9c0ef8aab322e7dbf487dbccc4c6
mikeyd <mdeguzis@gmail.com>
2015-05-01 09:01:50 -0400

    Update gpg_import.sh

--- extra/gpg_import.sh
+++ extra/gpg_import.sh
@@ -3,43 +3,13 @@
 # Author:     		Michael DeGuzis
 # Git:		      	https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	  	emu-from-source.sh
-# Script Ver:	  	0.2.5
+# Script Ver:	  	0.1.2
 # Description:		script to add and export a gpg key
 # -------------------------------------------------------------------------------
-
-# set vars
 key="$1"
 keyserver="hkp://subkeys.pgp.net"
-key_short=$(echo $key | cut -c 8-16)
 
-# name of key in check below is passed from previous script
-# echo $gpg_key_name
 
-# allow script to run in current DIR if not called previously
-if [[ "$scriptdir" != "" ]]; then
-  # set to scriptdir
-  gpg_cmd="$scriptdir/extra/gpg_import.sh $key"
-elif [[ "$pwd" != "$0" ]]; then
-  # if called from a dir tree, such as 'extra/gpg_import.sh', act accordingly.
-  gpg_cmd="$0 $key"
-else
-  # run from current dir
-  scriptdir=$(pwd)
-  gpg_cmd="$scriptdir/gpg_import.sh $key" 
-fi
-
-# check key first to avoid importing twice
-gpg_key_check=$(gpg --list-keys "$key_short")
-
-# evaluate
-if [[ "$gpg_key_check" != "" ]]; then
-  echo -e "\nGPG key "$key" [OK]\n"
-  sleep 1s
-else
-  echo -e "\nGPG key "$key" [FAIL]. Adding now...\n"
-  $gpg_cmd
-fi
-
-#Import 
+# import gpg key
 gpg --keyserver  $keyserver --recv-keys $key
 gpg -a --export $key | sudo apt-key add -

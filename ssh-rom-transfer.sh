723b8c1a052f7c9671d9d0dc10a14f4ee165971a
mikeyd <mdeguzis@gmail.com>
2015-05-02 12:20:41 -0400

    Create ssh-rom-transfer.sh

---
+++ ssh-rom-transfer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# -----------------------------------------------------------------------
+# Author: 	    Michael DeGuzis
+# Git:		      https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name: 	ssh-rom-transfer.sh
+# Script Ver: 	0.1.1
+# Description:	This script dumps ROMs over SSH
+#
+# Usage:	      ./ssh-rom-transfer.sh
+# ------------------------------------------------------------------------
+
+echo -e "\nEnter Remote User:"
+read user
+
+echo -e "\nEnter remote hostname:"
+read host
+
+echo -e "\nEnter remote DIR (use quotes on any single DIR name with spaces):"
+read remote_dir
+
+# Show remote list first
+echo -e "\nShowing remote listing first...press q to quit listing\n"
+sleep 2s
+
+ssh ${user}@${host} ls -l ${remote_dir} | less
+
+# copy ROMs
+sudo scp -r ${user}@${host}:'${remote_dir}' "/home/steam/ROMs"

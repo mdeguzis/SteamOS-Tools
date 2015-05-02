40d65f3cdc568865da5f4b57f4037251bd2a5b03
mikeyd <mdeguzis@gmail.com>
2015-05-02 12:51:26 -0400

    Update ssh-rom-transfer.sh

--- ssh-rom-transfer.sh
+++ ssh-rom-transfer.sh
@@ -44,10 +44,12 @@ ssh ${user}@${host} ls -l ${remote_dir} | less
 echo -e "\nEnter target ROM DIR to copy (use quotes on any single DIR name with spaces):"
 read target_dir
 
-# set full target path
-full_path=$(echo "$remote_dir/$target_dir")
-
 # copy ROMs
 echo -e "\nExecuting CMD: sudo scp -r $user@$host:'$full_path' /home/steam/ROMs/temp"
 sleep 1s
-sudo scp -r $user@$host:'${full_path}' /home/steam/ROMs/temp
+
+# set cmd
+CMD=$(echo "sudo scp -r $user@$host:'${full_path}' /home/steam/ROMs/temp")
+
+# execute
+"$CMD"

ff9ff7bea3ff3ad83f2d304ee297ac12af950f70
mikeyd <mdeguzis@gmail.com>
2015-05-02 18:25:37 -0400

    Update ssh-rom-transfer.sh

--- ssh-rom-transfer.sh
+++ ssh-rom-transfer.sh
@@ -115,7 +115,7 @@ fi
 echo -e "\nShowing remote listing first...press q to quit listing\n"
 sleep 2s
 
-ssh ${user}@${host} ls ${remote_dir}
+ssh ${user}@${host} ls ${remote_dir} | less
 
 echo -e "\nEnter target ROM DIR to copy [ENTER to last: $target_dir]:"
 echo -e "(use quotes on any single DIR name with spaces)"

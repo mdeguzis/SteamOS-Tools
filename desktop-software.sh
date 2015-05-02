f465ec53ce79596b65363a4c639540ccfa3fd4f2
mikeyd <mdeguzis@gmail.com>
2015-05-02 10:37:05 -0400

    disable manual dep install for ue4, trying auto

--- desktop-software.sh
+++ desktop-software.sh
@@ -339,7 +339,10 @@ get_software_type()
                 exit
         elif [[ "$type" == "ue4" ]]; then
                 # install ue4 from helper script
-                software_list="$scriptdir/cfgs/ue4.txt "
+                #software_list="$scriptdir/cfgs/ue4.txt"
+                # skip to ue4 module for now, setup.sh within that build
+                # script will attempt to get our source deps.
+                m_install_ue4_src(
         elif [[ "$type" == "$type" ]]; then
                 # install based on $type string response
 		software_list="custom-pkg.txt"

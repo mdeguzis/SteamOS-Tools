46314b53a6b61876f2c1d679e5fab8a383e42502
mikeyd <mdeguzis@gmail.com>
2015-05-02 12:37:20 -0400

    Update ssh-rom-transfer.sh

--- ssh-rom-transfer.sh
+++ ssh-rom-transfer.sh
@@ -9,6 +9,22 @@
 # Usage:	      ./ssh-rom-transfer.sh
 # ------------------------------------------------------------------------
 
+# prereqs
+	
+	# Adding repositories
+	PKG="openssh-server"
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $PKG | grep "install ok installed")
+	
+	if [ "" == "$PKG_OK" ]; then
+		echo -e "\n$PKG not found. Setting up $PKG.\n"
+		sleep 1s
+		sudo apt-get install $PKG
+	else
+		echo "Checking for $PKG [Ok]"
+		sleep 0.2s
+	fi
+
+
 echo -e "\nEnter Remote User:"
 read user
 

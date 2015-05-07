e85e30f40b660bd9ae4b3d676156ff9345d47897
mikeyd <mdeguzis@gmail.com>
2015-05-07 07:57:44 -0400

    rework upload script for looping

--- extra/upload-pkg-to-libregeek.sh
+++ extra/upload-pkg-to-libregeek.sh
@@ -7,7 +7,7 @@
 # Script Ver:	  	0.1.1
 # Description:		upload completed deb packages to libregeek.org
 #
-# Usage:          ./upload-pkg-to-libregeek.sh [pkg name]
+# Usage:          ./upload-pkg-to-libregeek.sh
 # -------------------------------------------------------------------------------
 
 TYPE="$1"
@@ -16,56 +16,65 @@ script_dir="$PWD"
 #Set defaults if user doesn't enter a TYPE
 sourcedir="/home/desktop/build-deb-temp/"
 
-funct_set_type()
+funct_set_vars()
 {
-
-  if [[ "$TYPE" == "emulation" ]]; then
-    # copy pkg to emulation on libregeek
-    sourcedir="/home/desktop/build-deb-temp/"
-    user="thelinu2"
-    host="libregeek.org"
-    destdir="/home2/thelinu2/public_html/SteamOS-Extra/emulation"
-    
-  elif [[ "$TYPE" == "emulation-src" ]]; then
-    # copy pkg to emulation on libregeek
-    sourcedir="/home/desktop/build-deb-temp/"
-    user="thelinu2"
-    host="libregeek.org"
-    # this destdir will move to a multimedia dir soon
-    destdir="/home2/thelinu2/public_html/SteamOS-Extra/emulation-src"
   
-  elif [[ "$TYPE" == "browser" ]]; then
-    # copy pkg to emulation on libregeek
-    sourcedir="/home/desktop/build-deb-temp/"
-    user="thelinu2"
-    host="libregeek.org"
-    # this destdir will move to a multimedia dir soon
-    destdir="/home2/thelinu2/public_html/SteamOS-Extra/browsers"
+  sourcedir="/home/desktop/build-deb-temp/"
+  user="thelinu2"
+  host="libregeek.org"
   
-  elif [[ "$TYPE" == "utlities" ]]; then
-    # copy pkg to emulation on libregeek
-    sourcedir="/home/desktop/build-deb-temp/"
-    user="thelinu2"
-    host="libregeek.org"
-    # this destdir will move to a multimedia dir soon
-    destdir="/home2/thelinu2/public_html/SteamOS-Extra/utlities"
+}
 
-  elif [[ "$TYPE" == "misc" ]]; then
-    # copy pkg to emulation on libregeek
-    sourcedir="/home/desktop/build-deb-temp/"
-    user="thelinu2"
-    host="libregeek.org"
-    # this destdir will move to a multimedia dir soon
-    destdir="/home2/thelinu2/public_html/SteamOS-Extra/misc"
-
-  elif [[ "$TYPE" == "multimedia" ]]; then
-    # copy pkg to emulation on libregeek
-    sourcedir="/home/desktop/build-deb-temp/"
-    user="thelinu2"
-    host="libregeek.org"
-    # this destdir will move to a multimedia dir soon
-    destdir="/home2/thelinu2/public_html/SteamOS-Extra/multimedia"
-  fi
+funct_set_dir()
+{
+  
+  # set path for upload
+	echo -e "\nPlease select which SteamOS-Extra folder you would like the package uploaded:" \n"
+	echo "(1) SteamOS-Extra/emulation"
+	echo "(2) SteamOS-Extra/emulation-src"
+	echo "(3) SteamOS-Extra/browser"
+	echo "(4) SteamOS-Extra/utilities"
+	echo "(5) SteamOS-Extra/misc"
+	echo "(6) SteamOS-Extra/Multimedia"
+	echo "(6) Default Public HTML"
+	echo ""
+	
+	# the prompt sometimes likes to jump above sleep
+	sleep 0.5s
+	
+	read -ep "Choice: " dir_choice
+	
+	case "$dir_choice" in
+	        
+	      1)
+	      destdir="/home2/thelinu2/public_html/SteamOS-Extra/emulation"
+	      ;;
+	      
+	      2)
+	      destdir="/home2/thelinu2/public_html/SteamOS-Extra/emulation-src"
+	      ;;
+	      
+	      3)
+	      destdir="/home2/thelinu2/public_html/SteamOS-Extra/browsers"
+	      ;;
+	      
+	      4)
+	      destdir="/home2/thelinu2/public_html/SteamOS-Extra/utlities"
+	      ;;
+	      
+	      5)
+	      destdir="/home2/thelinu2/public_html/SteamOS-Extra/misc"
+	      ;;
+	      
+	      6)
+	      destdir="/home2/thelinu2/public_html/SteamOS-Extra/multimedia"
+	      ;;
+	      
+	      7)
+	      destdir="/home2/thelinu2/public_html/
+	      ;;
+	      
+	esac
 
 }
 
@@ -76,22 +85,42 @@ funct_transfer()
   scp $sourcedir/$PKG $user@$host:$destdir
 }
 
+main()
+{
+  
+  clear
+  echo -e "\n==> Displaying contents of $sourcedir:\n"
+  sleep 2s
+  
+  ls "$sourcedir"
+  cd $sourcedir
+  echo ""
+  
+  echo -e "\nPlease enter your build commands, pressing [ENTER] after each one."
+  echo -e "When finished, please enter the word 'done' without quotes\n\n"
+  sleep 1s
+	
+	while [[ "$PKG" != "done" ]];
+	do
+		# capture command
+		read -ep "Package to upload >> " PKG
+		
+		# ignore executing src_cmd if "done"
+		if [[ "$PKG" == "done" ]]; then
+			# do nothing
+			echo "" > /dev/null
+    fi
+    
+    # set dir and transfer
+    funct_set_dir
+    funct_transfer
+    
+	done
+
+}
+
 ##############################################
 # main start
 ##############################################
-
-clear
-echo -e "\n==> Displaying contents of $sourcedir:\n"
-sleep 2s
-
-ls "$sourcedir"
-cd $sourcedir
-echo ""
-
-# get pkg from user
-read -ep "Package to upload >> " PKG
-
-# evaluate and process
-funct_set_type
-funct_transfer
+main
 

97b7a1da848aaa0ec1df0e110d39ae6e37d1c901
mikeyd <mdeguzis@gmail.com>
2015-05-08 12:27:57 -0400

    Create new file

---
+++ extra/build-pipelight-from-src.sh
@@ -0,0 +1,21 @@
+# -------------------------------------------------------------------------------
+# Author:       	Michael DeGuzis
+# Git:		        https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	    build-pipelight-from-src.sh
+# Script Ver:	    0.1.1
+# Description:  	Attempts to build pipelight from src
+#
+# Usage:          ./build-pipelight-from-src.sh
+#
+# Warning:	      You MUST have the Debian repos added properly for
+#	      	        Installation of the pre-requisite packages.
+# -------------------------------------------------------------------------------
+
+main()
+{
+
+  echo ""
+
+}
+
+# Start main script

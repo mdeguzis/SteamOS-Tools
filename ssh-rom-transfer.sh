43e360e4de7ad601bd904ef816763996852f09a7
mikeyd <mdeguzis@gmail.com>
2015-05-09 12:22:31 -0400

    look and feel updates

--- ssh-rom-transfer.sh
+++ ssh-rom-transfer.sh
@@ -39,9 +39,11 @@ fi
 
 ssh_transfer_roms()
 {
+	
+	# vars
+	ROMS_DIR="/home/steam/ROMs"
 
-# prereqs
-
+	# prereqs
 	clear
 	# Adding repositories
 	PKG="openssh-server"
@@ -57,100 +59,99 @@ ssh_transfer_roms()
 	fi
 
 
-echo -e "\n==> Enter Remote User: [ENTER to use last: $user]"
-
-# set tmp var for last run, if exists
-user_tmp="$user"
-if [[ "$user" == "" ]]; then
-	# var blank this run, get input
-	read user
-else
-	read user
-	# user chose to keep var value from last
+	echo -e "\n==> Enter Remote User: [ENTER to use last: $user]"
+	
+	# set tmp var for last run, if exists
+	user_tmp="$user"
 	if [[ "$user" == "" ]]; then
-		user="$user_tmp"
+		# var blank this run, get input
+		read user
 	else
-		# keep user choice
-		user="$user"
+		read user
+		# user chose to keep var value from last
+		if [[ "$user" == "" ]]; then
+			user="$user_tmp"
+		else
+			# keep user choice
+			user="$user"
+		fi
 	fi
-fi
 
-
-echo -e "\n==> Enter remote hostname: [ENTER to use last: $host]"
-# set tmp var for last run, if exists
-host_tmp="$host"
-if [[ "$host" == "" ]]; then
-	# var blank this run, get input
-	read host
-else
-	read host
-	# user chose to keep var value from last
+	echo -e "\n==> Enter remote hostname: [ENTER to use last: $host]"
+	# set tmp var for last run, if exists
+	host_tmp="$host"
 	if [[ "$host" == "" ]]; then
-		host="$host_tmp"
+		# var blank this run, get input
+		read host
 	else
-		# keep user choice
-		host="$host"
+		read host
+		# user chose to keep var value from last
+		if [[ "$host" == "" ]]; then
+			host="$host_tmp"
+		else
+			# keep user choice
+			host="$host"
+		fi
 	fi
-fi
 
-echo -e "\n==> Enter remote DIR: [ENTER to use last: $remote_dir]:"
-echo -e "(use quotes on any single DIR name with spaces)"
-# set tmp var for last run, if exists
-remote_dir_tmp="$remote_dir"
-if [[ "$remote_dir" == "" ]]; then
-	# var blank this run, get input
-	read remote_dir
-else
-	read remote_dir
-	# user chose to keep var value from last
+	echo -e "\n==> Enter remote DIR: [ENTER to use last: $remote_dir]:"
+	echo -e "(use quotes on any single DIR name with spaces)"
+	# set tmp var for last run, if exists
+	remote_dir_tmp="$remote_dir"
 	if [[ "$remote_dir" == "" ]]; then
-		remote_dir="$remote_dir_tmp"
+		# var blank this run, get input
+		read remote_dir
 	else
-		# keep user choice
-		remote_dir="$remote_dir"
+		read remote_dir
+		# user chose to keep var value from last
+		if [[ "$remote_dir" == "" ]]; then
+			remote_dir="$remote_dir_tmp"
+		else
+			# keep user choice
+			remote_dir="$remote_dir"
+		fi
 	fi
-fi
 
-# Show remote list first
-echo -e "\n==> Showing remote listing first...press q to quit listing\n"
-sleep 2s
-
-ssh ${user}@${host} ls ${remote_dir} | less
-
-echo -e "\nEnter target ROM DIR to copy [ENTER to last: $target_dir]:"
-echo -e "(use quotes on any single DIR name with spaces)"
-# set tmp var for last run, if exists
-target_dir_tmp="$target_dir"
-if [[ "$target_dir" == "" ]]; then
-	# var blank this run, get input
-	read target_dir
-else
-	read target_dir
-	# user chose to keep var value from last
+	# Show remote list first
+	echo -e "\n==> Showing remote listing first...press q to quit listing\n"
+	sleep 2s
+	
+	ssh ${user}@${host} ls ${remote_dir} | less
+	
+	echo -e "\nEnter target ROM DIR to copy [ENTER to last: $target_dir]:"
+	echo -e "(use quotes on any single DIR name with spaces)"
+	# set tmp var for last run, if exists
+	target_dir_tmp="$target_dir"
 	if [[ "$target_dir" == "" ]]; then
-		remote_dir="$target_dir_tmp"
+		# var blank this run, get input
+		read target_dir
 	else
-		# keep user choice
-		target_dir="$target_dir"
+		read target_dir
+		# user chose to keep var value from last
+		if [[ "$target_dir" == "" ]]; then
+			remote_dir="$target_dir_tmp"
+		else
+			# keep user choice
+			target_dir="$target_dir"
+		fi
 	fi
-fi
 
-# set globbed path
-# We need to keep the backslash
-# example in bash ... t="Neo\ Geo" && s=$(echo $t) && echo $s
-# target_dir=$(echo $target_dir)
-
-# copy ROMs
-echo -e "\n==> Executing CMD: sudo scp -r $user@$host:'$remote_dir/$target_dir' /home/steam/ROMs"
-sleep 1s
-
-# set cmd
-CMD=$(echo "sudo scp -r $user@$host:'$remote_dir/$target_dir' /home/steam/ROMs/temp")
-
-# execute
-echo ""
-$CMD
-echo ""
+	# set globbed path
+	# We need to keep the backslash
+	# example in bash ... t="Neo\ Geo" && s=$(echo $t) && echo $s
+	# target_dir=$(echo $target_dir)
+	
+	# copy ROMs
+	echo -e "\n==> Executing CMD: sudo scp -r $user@$host:'$remote_dir/$target_dir/' $ROM_DIR"
+	sleep 1s
+	
+	# set cmd
+	CMD=$(echo "sudo scp -r $user@$host:'$remote_dir/$target_dir/' $ROM_DIR")
+	
+	# execute
+	echo ""
+	$CMD
+	echo ""
  
 }
 

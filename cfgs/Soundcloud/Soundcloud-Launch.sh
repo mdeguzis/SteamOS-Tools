bea02056b8ae1780f3ffe2f6d13cb9baf8f54d70
mikeyd <mdeguzis@gmail.com>
2015-05-23 10:16:20 -0400

    Update Soundcloud-Launch.sh

--- cfgs/Soundcloud/Soundcloud-Launch.sh
+++ cfgs/Soundcloud/Soundcloud-Launch.sh
@@ -9,7 +9,7 @@ main()
 	/usr/bin/Xephyr :15 -ac -screen $WIN_RES -fullscreen -host-cursor -once & XEPHYR_PID=$!
 
 	export DISPLAY=:15
-	LD_PRELOAD= google-chrome --kiosk https://www.netflix.com/ --window-size=$COMMA_WIN_RES &&
+	LD_PRELOAD= google-chrome --kiosk www.soundcloud.com --window-size=$COMMA_WIN_RES &&
 
 	sleep 1
 	killall chrome

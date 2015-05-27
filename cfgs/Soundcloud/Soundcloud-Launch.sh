7a3dc2bdd902411b651ed42f305593d0dee81e86
mikeyd <mdeguzis@gmail.com>
2015-05-27 19:10:39 -0400

    placeholder for antimicro mouse control

--- cfgs/Soundcloud/Soundcloud-Launch.sh
+++ cfgs/Soundcloud/Soundcloud-Launch.sh
@@ -7,11 +7,12 @@ main()
 	COMMA_WIN_RES=$(echo $WIN_RES | awk '{sub(/x/, ","); print}')
 
 	/usr/bin/Xephyr :15 -ac -screen $WIN_RES -fullscreen -host-cursor -once & XEPHYR_PID=$!
+	
+	# start antimicro mouse control
+        #antimicro_temp
 
 	export DISPLAY=:15
 	LD_PRELOAD= google-chrome --kiosk www.soundcloud.com --window-size=$COMMA_WIN_RES &&
-
-	#antimicro_temp
 
 	sleep 1
 	killall chrome

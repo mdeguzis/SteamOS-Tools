f965866f608f37feb1646fb0a0775f6c31c40d5f
mikeyd <mdeguzis@gmail.com>
2015-06-03 08:59:10 -0400

    add kill by PID for antimicro

--- cfgs/Twitch/Twitch-Launch.sh
+++ cfgs/Twitch/Twitch-Launch.sh
@@ -10,13 +10,14 @@ main ()
 
 	# start antimicro mouse control
         #antimicro_tmp
+        antimicro_PID=$!
 
 	export DISPLAY=:15
 	LD_PRELOAD= google-chrome --kiosk www.twitch.tv --window-size=$COMMA_WIN_RES &&
 
 	sleep 1
 	killall chrome
-	killall antimicro
+	kill $antimicro_PID
 	kill $XEPHYR_PID
 
 }

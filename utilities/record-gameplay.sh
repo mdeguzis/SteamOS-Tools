595b2f86569ec3451c90b751af404079c2c3ad7a
ProfessorKaos64 <mdeguzis@gmail.com>
2015-06-05 19:23:02 -0400

    mark gameplay record exec

---
+++ utilities/record-gameplay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------
+# Author:         	Michael DeGuzis
+# Git:		        https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	      	record-gameplay.sh
+# Script Ver:	        0.0.1
+# Description:	    	Record gameplay from SteamOS runngin BPM over SSH
+#
+# Usage:	        ./record-gameplay.sh
+#
+# Warning:	        You MUST have the Debian repos added properly for
+#	                Installation of the pre-requisite packages.
+# -------------------------------------------------------------------------------

158089991de85540148b4f390c471da11118e831
mikeyd <mdeguzis@gmail.com>
2015-06-05 17:08:56 -0400

    Update brutal-doom.sh

--- ext-game-installers/brutal-doom.sh
+++ ext-game-installers/brutal-doom.sh
@@ -6,7 +6,8 @@
 # Scipt Name:	      brutal-doom.sh
 # Script Ver:	      0.0.1
 # Description:	    Installs the latest Brutal Doom under Linux / SteamOS
-#
+#                   Based off of https://github.com/coelckers/gzdoom
+#                   Compile using CMake.
 #
 # Usage:	          ./dbrutal-doom.sh
 # Help:         		./desktop-software.sh --help
@@ -15,10 +16,18 @@
 #	                	Installation of the pre-requisite packages.
 # -------------------------------------------------------------------------------
 
+# set scriptdir
+scriptdir="/home/desktop/SteamOS-Tools"
+
 ############################################
 # vars
 ############################################
+
 brutal_dir="/home/steam/brutal-doom"
+
+# GPG key for building using "build-deb-from-ppa"
+# deb-src http://archive.getdeb.net/ubuntu vivid-getdeb apps
+# A8A515F046D7E7CF
 
 ############################################
 # Prerequisite packages

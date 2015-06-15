264aced6ca6a55afbf751111070829ec65b53498
mikeyd <mdeguzis@gmail.com>
2015-06-14 20:33:23 -0400

    Update add-wine-game.sh

--- utilities/wine/add-wine-game.sh
+++ utilities/wine/add-wine-game.sh
@@ -9,5 +9,51 @@
 # See:            http://steamcommunity.com/groups/steamuniverse/
 #                 discussions/1/618463738398679174/
 #
-# Usage:		      wine-game-launch.sh
+# Usage:		      TODO
 # ------------------------------------------------------------------------
+
+################################
+# WARNING!!!
+################################
+# BEING TESTED / WORKED ON
+# DO NOT USE
+################################
+
+# vars
+GAME_ID="default"
+GAME_LAUNCHER_TMP="default"
+WG_DIR="/home/steam/wgstarts"
+WINE_LAUNCH_DIR="/home/wine/wingame"
+
+# create WG_DIR DIR if it does not exist
+# "wine game starts"
+
+# create game launch script
+
+# prompt for game name, store to 'game_id'
+
+# set game launcher name based on game id
+GAME_LAUNCHER_TMP="${GAME_ID}-Launcher.sh"
+
+# echo script text into /home/steam/wgstarts
+cat <<-EOF > ${WG_DIR}/${GAME_LAUNCHER_TMP}
+install -m 777 /dev/null /tmp/wgname
+echo $GAME_ID >> /tmp/wgname
+feh -F -b -x --auto-zoom /usr/share/backgrounds/cog.png &
+/usr/bin/switchtowine.sh
+sleep 4
+killall feh
+EOF
+
+# create desktop file from skel
+
+# create actual wine launcher under /home/steam/wingame
+# this may exist, I need to test first with a Crossover install
+
+cat <<-EOF > ${WINE_LAUNCH_DIR}${GAME_LAUNCHER_TMP}
+#!/bin/bsh
+sh /home/wine/shutdown-wine-game.sh &
+exec "/opt/cxoffice/bin/wine" --bottle "Steam" --check --wait-children --start "/home/wine/.cxoffice/Steam/drive_c/users/crossover/Desktop/Leviathan Warships.url" "$@"
+killall steam.exe
+EOF
+

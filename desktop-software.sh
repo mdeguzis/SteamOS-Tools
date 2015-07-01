251d1895566156b3a62282ced6002db7b93ec16e
mikeyd <mdeguzis@gmail.com>
2015-07-01 19:56:24 -0400

    update gpg key for debian archive

--- desktop-software.sh
+++ desktop-software.sh
@@ -369,23 +369,11 @@ function gpg_import()
 	# Full Key ID: 7638D0442B90D010
 	gpg_key_check=$(gpg --list-keys 2B90D010)
 	if [[ "$gpg_key_check" != "" ]]; then
-		echo -e "\nDebian Archive Automatic Signing Key [OK]"
-		sleep 0.3s
-	else
-		echo -e "\nDebian Archive Automatic Signing Key [FAIL]. Adding now..."
-		$scriptdir/utilities/gpg_import.sh 7638D0442B90D010
-	fi
-	
-	# Key Desc: Debian Archive Automatic Signing Key
-	# Key ID: 65558117
-	# Full Key ID: 5C808C2B65558117
-	gpg_key_check=$(gpg --list-keys 65558117)
-	if [[ "$gpg_key_check" != "" ]]; then
 		echo -e "Debian Multimeda Signing Key [OK]"
 		sleep 0.3s
 	else
 		echo -e "Debian Multimeda Signing Key [FAIL]. Adding now..."
-		$scriptdir/utilities/gpg_import.sh 5C808C2B65558117
+		$scriptdir/utilities/gpg_import.sh 7638D0442B90D010
 	fi
 
 }

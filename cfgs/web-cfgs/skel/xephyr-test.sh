e8d84dafd7864678015633f907192c540008c60c
mikeyd <mdeguzis@gmail.com>
2015-08-18 12:34:06 -0400

    Create new file

---
+++ cfgs/web-cfgs/skel/xephyr-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------
+# Author:    		Michael DeGuzis
+# Git:		    	https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	  xephyr-test.sh
+# Script Ver:		0.1.1
+# Description:	Basic test with xephyr to draw a display window
+#
+# Usage:      	xephyr-test.sh
+#               non-Steam addition of Xephyr-Test desktop shortcut
+# -------------------------------------------------------------------------------
+

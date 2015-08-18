f184c419b6574b94edc64df2bd15e1b0c558e5c6
mikeyd <mdeguzis@gmail.com>
2015-08-18 14:43:02 -0400

    Update xephyr-test.sh

--- cfgs/web-cfgs/xephyr-test.sh
+++ cfgs/web-cfgs/xephyr-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -------------------------------------------------------------------------------
-# Author:    		Michael DeGuzis
-# Git:		    	https://github.com/ProfessorKaos64/SteamOS-Tools
+# Author:       Michael DeGuzis
+# Git:          https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:   xephyr-test.sh
 # Script Ver:   0.1.1
 # Description:  Basic test with xephyr/xnest to draw a display window

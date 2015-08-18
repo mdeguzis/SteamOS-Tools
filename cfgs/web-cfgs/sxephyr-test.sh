6dd38b14ae0293ea619c1f61dcc4c456ea69c180
mikeyd <mdeguzis@gmail.com>
2015-08-18 12:41:16 -0400

    Update xephyr-test.sh

--- cfgs/web-cfgs/sxephyr-test.sh
+++ cfgs/web-cfgs/sxephyr-test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # -------------------------------------------------------------------------------
 # Author:    		Michael DeGuzis
 # Git:		    	https://github.com/ProfessorKaos64/SteamOS-Tools
@@ -9,5 +8,19 @@
 #
 # Usage:      	xephyr-test.sh
 #               non-Steam addition of Xephyr-Test desktop shortcut
+#
+# See:          http://jeffskinnerbox.me/posts/2014/Apr/29/howto-using-xephyr-to-
+#               create-a-new-display-in-a-window/
 # -------------------------------------------------------------------------------
+
+# pre-reqs
+sudo apt-get install xephyr xnest
+
+# Copy desktop file over for use in BPM
+cp "Xephyr-Test.desktop" "/usr/share/applications"
+
+# Xnest basic test
+Xnest :3 -geometry 1280x1024+200+200 -name "Xnest Test Window" 2> /dev/null & xclock -display :3 &
+killall Xnest
+
 

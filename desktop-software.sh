f8df9d077a092b47ce6270afc3a971a68aee6286
mikeyd <mdeguzis@gmail.com>
2015-08-20 22:24:54 -0400

    Update desktop-software.sh

--- desktop-software.sh
+++ desktop-software.sh
@@ -4,7 +4,7 @@
 # Author: 	Michael DeGuzis
 # Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	install-desktop-software.sh
-# Script Ver:	1.9.8.7
+# Script Ver:	1.9.8.8
 # Description:	Adds various desktop software to the system for a more
 #		usable experience. Although this is not the main
 #		intention of SteamOS, for some users, this will provide

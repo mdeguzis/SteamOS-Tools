bf5f609b4e52813a27db9256c6e58ed377e2aeb0
mikeyd <mdeguzis@gmail.com>
2015-08-23 16:23:10 -0400

    Update window-test.sh

--- cfgs/web-cfgs/window-test.sh
+++ cfgs/web-cfgs/window-test.sh
@@ -31,33 +31,15 @@ WIN_RES=$(DISPLAY=:0 xdpyinfo | grep dimensions | awk '{print $2}')
 ###################################################################
 # Test scenarios
 ###################################################################
+WIN_RES=$(DISPLAY=:0 xdpyinfo | grep dimensions | awk '{print $2}')
+COMMA_WIN_RES=$(echo $WIN_RES | awk '{sub(/x/, ","); print}')
 
-# Each section below can test a certain method. Uncomment the method
-# you wish to test. Display resolutions are specified manually for now.
+Xephyr :15 -ac -screen 1920x1080 -fullscreen -host-cursor -once &
+export DISPLAY=:15
+openbox-session &
+LP=$LD_PRELOAD
+unset LD_PRELOAD
+google-chrome http://www.youtube.com/tv#/ --kiosk --windows-size=1920,1080
+export LD_PRELOAD=$LP
+killall Xephyr
 
-# (1) Xnest basic test
-#setsid sh -c 'exec google-chrome <> /dev/tty2 >
-
-# WORKS:
-# Xnest :3 -geometry 1280x1024+200+200 -name "Xnest Test Window" 2> /dev/null & xclock -display :3 &
-
-# DOES NOT WORK:
-# Xnest :3 -geometry 1280x1024+200+200 -name "Xnest Test Window" 2> /dev/null & google-chrome -display :3 &
-
-# (2) Xephyr test
-# Xephyr -ac -screen $WIN_RES -br -reset -terminate 2> /dev/null :3 &
-#DISPLAY=:3.0 google-chrome --kiosk www.google.com &
-# sleep 10s
-# killall Xephyr
-#killall google-chrome
-
-# (3) test using gnome desktop
-# Xephyr -ac -screen 1280x1024 -br -reset -terminate 2> /dev/null :3 & \
-# DISPLAY=:3 gnome-session & DISPLAY=:3.0 ssh -XfC dekstop@steamos xterm
-# sleep 1
-# killall Xephyr
-
-# (4) xterm/openbox tests
-startx xterm -- :1 vt6
-sleep 10s
-exit 1

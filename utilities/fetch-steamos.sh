21bcc26ffd86edd6a729854173e3d0a6c7936ecf
mikeyd <mdeguzis@gmail.com>
2015-08-27 08:40:59 -0400

    Update fetch-steamos.sh

--- utilities/fetch-steamos.sh
+++ utilities/fetch-steamos.sh
@@ -15,68 +15,103 @@ clear
 
 download_release()
 {
-  
-  echo -e "==> Fetching $release\n"
-  
-  if [[ -f "$download_dir/SteamOS.DVD.iso" 
-     && -f "$download_dir/SteamOSInstaller.zip"
-     ]]; then
-    echo -e "$release release installer files found, overwrite?"
-    
-    # get user choice
-  	read -erp "Choice: " choice
-  	
-    if [[ "choice" == "y" ]]; then
-      wget -r "http://repo.steampowered.com/download/$release/*"
-    else
-      echo -e "Skipping download\n"
-    fi
-    
-  else
-  
-    echo -e "$release Release not found in target directory." 
-    echo -e "Downloading to: $download_dir"
-    sleep 2s
-    cd "$download_dir"
-    echo "fetching http://repo.steampowered.com/download/$release/"
-    wget --no-parent --recursive --no-directories --reject "index.html*" \
-    --no-clobber http://repo.steampowered.com/download/$release/
-  
-  fi
-  
-}
 
-pre_reqs()
-{
-  #####################
-  # directories
-  #####################
-  
   # check fo existance of dirs
-  
-  if [[ ! -d "$download_dir" ]]; then
-    mkdir -p "$download_dir"
+  if [[ ! -d "$HOME/downloads/$release" ]]; then
+    mkdir -p "$HOME/downloads/$release"
   fi
-
+  
+  # check for file existance
+  if [[ -f "$HOME/downloads/$release/$file" ]]; then
+    
+    echo -e "\nFile exists, overwrite? (y/n)"
+    read -ep "Choice: " dl_choice
+  
+  fi
+  
+  case "$dl_choice" in
+    
+    y)
+    # download requested file
+    cd "$HOME/downloads/$release"
+    wget --no-parent --recursive --no-directories --reject "index.html*" \
+    --no-clobber http://repo.steampowered.com/download/$release/$file
+    
+    # download MD5 or SHA file
+    if [[ "$file" == "SteamOSInstaller.zip" ]]; then
+    
+      rm -f "$HOME/downloads/$release/MD5SUM"
+      wget "$base_url/$release/MD5SUMS"
+      # replace downlaod location in file
+      sed -i 'g|/var/www/download|$HOME/downloads/$release|g' "$HOME/downloads/$release/MD5SUM"
+      
+    fi    
+      
+    ;;
+    
+    n)
+    echo -e "Aborting..."
+    clear
+    exit 1
+    ;;
+  
+  esac
+  
 }
 
 main()
 {
+    # set base URL
+    base_url="repo.steampowered.com/download"
+    base_dir="$HOME/downloads"
   
-  # remove any duplicated files. If something went wrong and we ended
-  # up with, for instance, SteamOSDVD.iso.1, let's remove that
-  #find "$download_dir/alchemist" -name *.so | xargs sudo cp -t "/usr/lib/libretro" 2> /dev/null
+  	# prompt user if they would like to load a controller config
+  	echo -e "\nPlease choose a release to download. Releases checked for integrity: \n"
+  	echo "(1) Alchemist (standard zip, UEFI only)"
+  	echo "(2) Alchemist (legacy ISO, BIOS systems)"
+  	echo "(3) Brewmaster (standard zip, UEFI only)"
+  	echo "(4) Brewmaster (legacy ISO, BIOS systems)"
+  	echo ""
+  	echo ""
+  	
+  	# the prompt sometimes likes to jump above sleep
+	sleep 0.5s
+	
+	read -ep "Choice: " rel_choice
+	
+  case "$rel_choice" in
+    1)
+    release="alchemist"
+    file="$base_url/SteamOSInstaller.zip"
+    download_release
+    check_integrity
+    ;;
+    
+    2)
+    
+    ;;
+    
+    3)
+    
+    ;;
+    
+    4)
+    
+    
+    *)
+    echo "Invalid Input, exiting"
+    exit 1
+    ;;
+    
+	esac
+ 
+} 
+
+check_download_integrity()
+{
   
-  # dowload alchemist
-  release="alchemist"
-  download_dir="/home/$USER/downloads/alchemist"
-  pre_reqs
-  download_release
-  
-  # download brewmaster
-  #release="brewmaster"
-  #pre_reqs
-  #download_dir="/home/$USER/downloads/brewmaster"
+  echo -e "\n==> Checking integrity of installer\n"
+  MD5SUM -c "$HOME/downloads/$release/$file" "$HOME/downloads/$release/MD5SUM"
   
 }
 

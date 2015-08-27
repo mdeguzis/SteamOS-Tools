bbc71f739ae31ee41b58df690b08288ed4a95c67
mikeyd <mdeguzis@gmail.com>
2015-08-27 08:52:53 -0400

    Update fetch-steamos.sh

--- utilities/fetch-steamos.sh
+++ utilities/fetch-steamos.sh
@@ -11,52 +11,63 @@
 # Usage:      		./fetch-steamos.sh
 # -------------------------------------------------------------------------------
 
-clear
+check_download_integrity()
+{
+  
+  echo -e "\n==> Checking integrity of installer\n"
+  
+  echo -e "\nMD5 Check:"
+  MD5SUM -c "$HOME/downloads/$release/$file" "$HOME/downloads/$release/MD5SUM"
+  
+  echo -e "\nSHA512 Check:"
+  SHA512SUM -c "$HOME/downloads/$release/$file" "$HOME/downloads/$release/SHA512SUMS"
+  
+}
+
+check_file_existance()
+{
+	
+	# check fo existance of dirs
+	if [[ ! -d "$HOME/downloads/$release" ]]; then
+		mkdir -p "$HOME/downloads/$release"
+	fi
+  
+	# check for file existance
+	if [[ -f "$HOME/downloads/$release/$file" ]]; then
+	
+		echo -e "\nFile exists, overwrite? (y/n)"
+		read -ep "Choice: " dl_choice
+		
+		if [[ "$dl_choice" == "y" ]]; then
+			download_release
+		else
+			echo -e "\nAborting..."
+			clear
+			exit 1
+		fi
+
+  	else
+	
+}
 
 download_release()
 {
+	
+	# download requested file
+	cd "$HOME/downloads/$release"
+	wget --no-parent --recursive --no-directories --reject "index.html*" \
+	--no-clobber http://repo.steampowered.com/download/$release/$file
+	
+	# download MD5 and SHA files
+	rm -f "$HOME/downloads/$release/MD5SUM"
+	rm -f "$HOME/downloads/$release/SHAD512SUMS"
+	
+	wget "$base_url/$release/MD5SUMS"
+	wget "$base_url/$release/SHA512SUMS"
 
-  # check fo existance of dirs
-  if [[ ! -d "$HOME/downloads/$release" ]]; then
-    mkdir -p "$HOME/downloads/$release"
-  fi
-  
-  # check for file existance
-  if [[ -f "$HOME/downloads/$release/$file" ]]; then
-    
-    echo -e "\nFile exists, overwrite? (y/n)"
-    read -ep "Choice: " dl_choice
-  
-  fi
-  
-  case "$dl_choice" in
-    
-    y)
-    # download requested file
-    cd "$HOME/downloads/$release"
-    wget --no-parent --recursive --no-directories --reject "index.html*" \
-    --no-clobber http://repo.steampowered.com/download/$release/$file
-    
-    # download MD5 or SHA file
-    if [[ "$file" == "SteamOSInstaller.zip" ]]; then
-    
-      rm -f "$HOME/downloads/$release/MD5SUM"
-      wget "$base_url/$release/MD5SUMS"
-      # replace downlaod location in file
-      sed -i 'g|/var/www/download|$HOME/downloads/$release|g' "$HOME/downloads/$release/MD5SUM"
-      
-    fi    
-      
-    ;;
-    
-    n)
-    echo -e "Aborting..."
-    clear
-    exit 1
-    ;;
-  
-  esac
-  
+	# replace download location in integrity check files
+	sed -i 'g|/var/www/download|$HOME/downloads/$release|g' "$HOME/downloads/$release/MD5SUM"
+	sed -i 'g|/var/www/download|$HOME/downloads/$release|g' "$HOME/downloads/$release/SHA512SUMS"
 }
 
 main()
@@ -66,7 +77,7 @@ main()
     base_dir="$HOME/downloads"
   
   	# prompt user if they would like to load a controller config
-  	echo -e "\nPlease choose a release to download. Releases checked for integrity: \n"
+  	echo -e "\nPlease choose a release to download. Releases checked for integrity \n"
   	echo "(1) Alchemist (standard zip, UEFI only)"
   	echo "(2) Alchemist (legacy ISO, BIOS systems)"
   	echo "(3) Brewmaster (standard zip, UEFI only)"
@@ -83,6 +94,7 @@ main()
     1)
     release="alchemist"
     file="$base_url/SteamOSInstaller.zip"
+    check_file_existance
     download_release
     check_integrity
     ;;
@@ -106,14 +118,6 @@ main()
 	esac
  
 } 
-
-check_download_integrity()
-{
-  
-  echo -e "\n==> Checking integrity of installer\n"
-  MD5SUM -c "$HOME/downloads/$release/$file" "$HOME/downloads/$release/MD5SUM"
-  
-}
 
 # Start script
 main

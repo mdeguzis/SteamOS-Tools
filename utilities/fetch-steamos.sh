e84865d64b8c6a34bf1133b6a86ad4bf37ec6184
mikeyd <mdeguzis@gmail.com>
2015-08-27 13:10:45 -0400

    testing

--- utilities/fetch-steamos.sh
+++ utilities/fetch-steamos.sh
@@ -152,24 +152,22 @@ download_release()
 		sed -i "s|SteamOSDVD.iso|$iso_new|g" "$HOME/downloads/$release/MD5SUMS"
 		sed -i "s|SteamOSInstaller.zip|$zip_new|g" "$HOME/downloads/$release/MD5SUMS"
 		
-		# remove SteamOSImage.zip, not needed
-		sed -i '/SteamOSImage.zip/d' "$HOME/downloads/$release/MD5SUMS"
-		
 	elif [[ "$release" == "brewmaster" ]]; then
 	
-		orig_prefix="  /var/www/download"
-		new_prefix="  $HOME/downloads/$release"
+		orig_prefix="/var/www/download"
+		new_prefix="$HOME/downloads/$release"
 		
 		sed -i "s|$orig_prefix|$new_prefix|g" "$HOME/downloads/$release/SHA512SUMS"
 		
 	fi
 	
 	# remove MD512/SHA512 line that does not match our file so we don't get check errors
+	
 	#trim_md512sum=$(grep -v $file "$HOME/downloads/$release/MD5SUMS")
 	#trim_sha512sum=$(grep -v $file "$HOME/downloads/$release/SHA512SUMS")
 	
-	sed '/$file/!d' "$HOME/downloads/$release/MD5SUMS"
-	sed '/$file/!d' "$HOME/downloads/$release/SHA512SUMS"
+	sed -i '/$file/!d' "$HOME/downloads/$release/MD5SUMS"
+	sed -i '/$file/!d' "$HOME/downloads/$release/SHA512SUMS"
 	
 }
 

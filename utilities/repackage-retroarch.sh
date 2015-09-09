9fde0532f9b3f2c6104a84812579a671f58b9116
mikeyd <mdeguzis@gmail.com>
2015-09-09 18:05:07 -0400

    Update repackage-retroarch.sh

--- utilities/repackage-retroarch.sh
+++ utilities/repackage-retroarch.sh
@@ -1,15 +1,15 @@
 # -------------------------------------------------------------------------------
-# Author:     	Michael DeGuzis
-# Git:		      https://github.com/ProfessorKaos64/SteamOS-Tools
+# Author:	Michael DeGuzis
+# Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name: 	repackage-retroarch.sh
-# Script Ver:	  0.1.1
+# Script Ver:	0.1.5
 # Description:	Overall goal of script is to automate rebuilding pkgs from
 #               https://launchpad.net/~libretro/+archive/ubuntu/stable?field.series_filter=trusty
 #
-# Usage:	      repackage-retroarch.sh
+# Usage:	repackage-retroarch.sh
 #
-# Warning:	    You MUST have the Debian repos added properly for
-#		            Installation of the pre-requisite packages.
+# Warning:	You MUST have the Debian repos added properly for
+#		nstallation of the pre-requisite packages.
 # -------------------------------------------------------------------------------
 
 # Should be able to make use of utilities/build-deb-from-ppa.sh

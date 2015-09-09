8a411ce74ea19d73017f3ced263e7b624f2283ed
mikeyd <mdeguzis@gmail.com>
2015-09-08 23:14:29 -0400

    Update bug-report-tool.sh

--- bug-report-tool.sh
+++ bug-report-tool.sh
@@ -11,8 +11,60 @@
 #
 # -------------------------------------------------------------------------------
 
+#############################################
 # pre-reqs
-sudo apt-get install git
+#############################################
+sudo apt-get install git lib32gcc1
+
+#############################################
+# Steam CMD
+#############################################
+
+echo -e "==> Installing SteamCMD"
+sleep 2s
+
+# check for SteamCMD's existance in /home/desktop
+if [[ ! -f "/home/desktop/steamcmd/steamcmd.sh" ]]; then
+	echo -e "\nsteamcmd not found\n"
+	echo -e "Attempting to install this now.\n"
+	sleep 1s
+	# if directory exists, remove it so we have a clean slate
+	if [[ ! -d "/home/desktop/steamcmd" ]]; then
+		rm -rf "/home/desktop/steamcmd"
+		mkdir ~/steamcmd
+	fi
+
+	# Download and unpack steamcmd directory
+	cd ~/steamcmd
+	wget "http://media.steampowered.com/installer/steamcmd_linux.tar.gz"
+	tar -xvzf steamcmd_linux.tar.gz
+
+	if [ $? == '0' ]; then
+		echo "Successfully installed 'steamcmd'"
+		sleep 2s
+	else
+		echo "Could not install 'steamcmd'. Exiting..."
+		sleep 2s
+		exit 1
+	fi
+else
+	echo "Checking for 'steamcmd' [Ok]"
+	sleep 0.2s
+fi
+
+#############################################
+# set vars
+#############################################
+
+# Steam-specific
+# There is a bug in the current steamcmd version that outputs a 
+# Danish "o" in "version"
+steam_ver=$(/home/desktop/steamcmd/steamcmd.sh "+versi$(echo -e '\xc3\xb8')n" +quit | grep "package" | cut -c 25-35)
+steam_api=$(/home/desktop/steamcmd/steamcmd.sh "+versi$(echo -e '\xc3\xb8')n" +quit | grep -E "^Steam API\:" | cut -c 12-15)
+
+#############################################
+# bug report generation
+#############################################
 
 # set bug report dir
 bug_dir="/home/desktop/bug-reports"
@@ -40,9 +92,15 @@ fi
 cd "$HOME/gist-cli"
 
 # some basic output to test:
-
 # CPU
 echo "-------------------------------------------------------" > bug.txt
+echo "Steam Info" >> bug.txt
+echo "-------------------------------------------------------" >> bug.txt
+echo $steam_ver >> bug.txt
+echo $steam_api >> bug.txt
+
+# CPU
+echo -e "\n-------------------------------------------------------" >> bug.txt
 echo "CPU Info" >> bug.txt
 echo "-------------------------------------------------------" >> bug.txt
 cat /proc/cpuinfo | grep -m3 -E 'model name|cpu cores|MHz' >> bug.txt

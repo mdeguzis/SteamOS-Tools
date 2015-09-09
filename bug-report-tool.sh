72e6fcc3ebf9ee27f6ce70b6e33edacb7961634c
mikeyd <mdeguzis@gmail.com>
2015-09-08 22:35:30 -0400

    Update bug-report-tool.sh

--- bug-report-tool.sh
+++ bug-report-tool.sh
@@ -30,21 +30,12 @@ timestamp=$(date +%Y%m%d-%H:%M:%S)
 bug_report_file="${bug_dir}/bug_report_${timestamp})"
 
 # lspci test
-cat lspci -v > $bug_report_file
-
+cat lspci -v > ${bug_report_file}
 
 cd
 git clone https://github.com/pranavk/gist-cli
 cd gist-cli
 chmod +x gistcli
-
-# create gist
-./gistcli -f $bug_report_file
-
-#gist_url=$(curl -sX POST --data-binary '{"files": {"file1.txt": {"content": "lspci -v"}}}' \
-#https://api.github.com/gists| grep "gist.github" | grep html_url | cut -c 16-59)
-
-# inform user to past url to git ticket
 
 cat <<- EOF
 -----------------------------------------------------------------
@@ -53,8 +44,10 @@ Summary
 Please paste the below URL into your SteamOS issues ticket at\n"
 https://github.com/ValveSoftware/SteamOS/issues
 
-URL: ${gist_url}
-
 EOF
 
-$gist
+# create gist
+./gistcli -f $bug_report_file
+
+#gist_url=$(curl -sX POST --data-binary '{"files": {"file1.txt": {"content": "lspci -v"}}}' \
+#https://api.github.com/gists| grep "gist.github" | grep html_url | cut -c 16-59)

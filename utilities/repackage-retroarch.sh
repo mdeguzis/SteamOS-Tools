bebe66bd0020569c53ff234a82521253d0d66063
mikeyd <mdeguzis@gmail.com>
2015-09-09 22:49:37 -0400

    Update repackage-retroarch.sh

--- utilities/repackage-retroarch.sh
+++ utilities/repackage-retroarch.sh
@@ -40,7 +40,8 @@ install_prereqs()
 	sleep 1s
 	# install needed packages
 	sudo apt-get install git devscripts build-essential checkinstall \
-	debian-keyring debian-archive-keyring cmake
+	debian-keyring debian-archive-keyring cmake libv4l-dev libusb-1.0-0-dev \
+	libopenal-dev libjack-jackd2-dev libgbm-dev python3-dev
 
 }
 

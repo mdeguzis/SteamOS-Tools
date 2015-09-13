5d43cf0a5127a29cd482eb9f98614506e5cdfc44
mikeyd <mdeguzis@gmail.com>
2015-09-13 08:05:59 -0400

    added deps, see summary, conflict currently...
    The following packages have unmet dependencies:
     libpostproc-dev : Depends: libpostproc53 (= 10:2.6.4-dmo1) but it is not going to be installed
                       Depends: libavutil-dev (>= 10:2.6.4-dmo1) but 6:11.4-1~deb8u1+bsos1 is to be installed
    
    submitting ticket to steamos tracker

--- utilities/repackage-kodi.sh
+++ utilities/repackage-kodi.sh
@@ -37,9 +37,23 @@ install_prereqs()
 	
 	sleep 1s
 	# install needed packages
-	sudo apt-get install git devscripts build-essential checkinstall \
-	debian-keyring debian-archive-keyring cmake libv4l-dev autotools-dev \
-	dh-autoreconf pkg-kde-tools doxygen graphviz gsfonts-x11
+	sudo apt-get install autopoint bison build-essential ccache cmake curl \
+	cvs default-jre fp-compiler gawk gdc gettext git-core gperf libasound2-dev \
+	libass-dev libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev \
+	libbluetooth-dev libbluray-dev libbluray1 libboost-dev libboost-thread-dev \
+	libbz2-dev libcap-dev libcdio-dev libcec-dev libcec1 libcrystalhd-dev libcrystalhd3 \
+	libcurl3 libcurl4-gnutls-dev libcwiid-dev libcwiid1 libdbus-1-dev libenca-dev \
+	libflac-dev libfontconfig-dev libfreetype6-dev libfribidi-dev libglew-dev \
+	libiso9660-dev libjasper-dev libjpeg-dev libltdl-dev liblzo2-dev libmad0-dev \
+	libmicrohttpd-dev libmodplug-dev libmp3lame-dev libmpeg2-4-dev libmpeg3-dev \
+	libmysqlclient-dev libnfs-dev libogg-dev libpcre3-dev libplist-dev libpng-dev \
+	libpostproc-dev libpulse-dev libsamplerate-dev libsdl-dev libsdl-gfx1.2-dev \
+	libsdl-image1.2-dev libsdl-mixer1.2-dev libshairport-dev libsmbclient-dev \
+	libsqlite3-dev libssh-dev libssl-dev libswscale-dev libtiff-dev libtinyxml-dev \
+	libtool libudev-dev libusb-dev libva-dev libva-egl1 libva-tpi1 libvdpau-dev \
+	libvorbisenc2 libxml2-dev libxmu-dev libxrandr-dev libxrender-dev libxslt1-dev \
+	libxt-dev libyajl-dev mesa-utils nasm pmount python-dev python-imaging python-sqlite \
+	swig unzip yasm zip zlib1g-dev
 	
 	echo -e "\n== Checking for extra standalone prequisite packages for batch building\n"
 	

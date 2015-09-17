942bdb796e65454981d215df141d1ac839b98a5e
mikeyd <mdeguzis@gmail.com>
2015-09-17 19:56:34 -0400

    spacing

--- desktop-software.sh
+++ desktop-software.sh
@@ -652,7 +652,7 @@ show_warning()
         if [[ "$sources_check_jessie" == "" || "$sources_check_steamos_tools" == "" ]]; then
                 echo -e " Those \nsources do *NOT* appear to be added at first glance!"
         else
-                echo -e " On \ninitial check, those sources appear to be added."
+                echo -e "\nOn initial check, those sources appear to be added."
         fi
                 
         echo -e "\nIf you wish to exit, please press CTRL+C now. Otherwise,\npress [ENTER] to continue."

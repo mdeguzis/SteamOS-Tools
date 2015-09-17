b5f6de3e441a6b97756a1e35f294ee477cd79315
mikeyd <mdeguzis@gmail.com>
2015-09-17 19:53:21 -0400

    enable gpg import

--- desktop-software.sh
+++ desktop-software.sh
@@ -804,7 +804,7 @@ main()
 		# load functions necessary for software actions
 		# GPG import should not be needed under brewmaster/Jessie
 		
-		# gpg_import
+		gpg_import
 		set_multiarch
 		pre_req_checks
 

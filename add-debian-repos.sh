732fcedafa8b572f64a77522ed235c3b111683db
mikeyd <mdeguzis@gmail.com>
2015-09-18 11:05:42 -0400

    gpg key error on deb multi media

--- add-debian-repos.sh
+++ add-debian-repos.sh
@@ -47,20 +47,20 @@ check_gpg()
 	else
 		echo -e "Libregeek Pacakge Signing Key [FAIL]. Adding now...\n"
 		sleep .5s
-		$scriptdir/utilities/gpg_import.sh 8106E72834C589A7 2> /dev/null
+		$scriptdir/utilities/gpg_import.sh 34C589A7 2> /dev/null
 	fi
 	
 	# Key Desc: Debian Multimedia
 	# Key ID: 34C589A7
 	# Full Key ID: 8106E72834C589A7
-	gpg_key_check=$(gpg --list-keys 6558117)
+	gpg_key_check=$(gpg --list-keys 65558117)
 	if [[ "$gpg_key_check" != "" ]]; then
 		echo -e "Deb Mulitmedia [OK]\n"
 		sleep 0.3s
 	else
 		echo -e "Deb Mulitmedia [FAIL]. Adding now...\n"
 		sleep .5s
-		$scriptdir/utilities/gpg_import.sh C808C2B6558117 2> /dev/null
+		$scriptdir/utilities/gpg_import.sh 65558117 2> /dev/null
 	fi
 	
 }

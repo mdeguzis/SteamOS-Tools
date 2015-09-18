ef52dec5754bc0251a418d437b866db386e0ea86
mikeyd <mdeguzis@gmail.com>
2015-09-17 20:11:07 -0400

    update to new key server

--- utilities/gpg_import.sh
+++ utilities/gpg_import.sh
@@ -3,14 +3,19 @@
 # Author:     		Michael DeGuzis
 # Git:		      	https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	  	emu-from-source.sh
-# Script Ver:	  	0.1.2
+# Script Ver:	  	0.1.3
 # Description:		script to add and export a gpg key
 # -------------------------------------------------------------------------------
-key="$1"
-keyserver="hkp://subkeys.pgp.net"
 
+# define key for argument
+key="$1"
+
+# Choose keyserver to use
+
+#keyserver="hkp://subkeys.pgp.net"
+keyserver="pgp.mit.edu"
 
 # import gpg key
-gpg --keyserver  $keyserver --recv-keys $key
+gpg --keyserver $keyserver --recv-keys $key
 gpg -a --export $key | sudo apt-key add -
 sudo apt-key update

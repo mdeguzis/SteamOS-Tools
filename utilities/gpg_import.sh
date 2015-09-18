530585a41a39b095c15926a0a3aa46f917d8cc74
mikeyd <mdeguzis@gmail.com>
2015-09-17 20:17:00 -0400

    supress output

--- utilities/gpg_import.sh
+++ utilities/gpg_import.sh
@@ -16,6 +16,6 @@ key="$1"
 keyserver="pgp.mit.edu"
 
 # import gpg key
-gpg --keyserver $keyserver --recv-keys $key
+gpg --keyserver $keyserver --recv-keys $key > /dev/null
 gpg -a --export $key | sudo apt-key add - >/dev/null
-sudo apt-key update
+sudo apt-key update > /dev/null

add4eaca2fa2d75b952fe2df34a7e71f73dda68a
mikeyd <mdeguzis@gmail.com>
2015-09-19 13:37:19 -0400

    add missing dep libcurl4-gnutls-dev

--- utilities/build-scripts/build-kodi-src.sh
+++ utilities/build-scripts/build-kodi-src.sh
@@ -79,8 +79,8 @@ install_prereqs()
 	default-jre gawk gperf libao-dev libasound2-dev \
 	libass-dev libavahi-client-dev libavahi-common-dev libbluetooth-dev \
 	libbluray-dev libboost-dev libboost-thread-dev libbz2-dev libcap-dev libcdio-dev \
-	libcec-dev libcurl3 libcwiid-dev libdbus-1-dev libfontconfig-dev libfreetype6-dev \
-	libfribidi-dev libgif-dev libglu1-mesa-dev \
+	libcec-dev libcurl3 libcurl4-gnutls-dev libcwiid-dev libdbus-1-dev libfontconfig-dev 
+	libfreetype6-dev libfribidi-dev libgif-dev libglu1-mesa-dev \
 	libiso9660-dev libjasper-dev libjpeg-dev libltdl-dev liblzo2-dev libmicrohttpd-dev \
 	libmodplug-dev libmpcdec-dev libmpeg2-4-dev libmysqlclient-dev libnfs-dev libogg-dev \
 	libpcre3-dev libplist-dev libpng12-dev libpng-dev libpulse-dev librtmp-dev libsdl2-dev \

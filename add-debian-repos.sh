aa8e6cc4d0b0c4a51aadfb25f7a19a050e01084e
mikeyd <mdeguzis@gmail.com>
2015-09-18 21:13:44 -0400

    update gpg method

--- add-debian-repos.sh
+++ add-debian-repos.sh
@@ -40,27 +40,27 @@ check_gpg()
 	# Key Desc: Libregeek Signing Key
 	# Key ID: 34C589A7
 	# Full Key ID: 8106E72834C589A7
-	gpg_key_check=$(gpg --list-keys 34C589A7) 
+	gpg_key_check=$(gpg --list-keys 8106E72834C589A7) 
 	if [[ "$gpg_key_check" != "" ]]; then
 		echo -e "Libregeek Pacakge Signing Key [OK]"
 		sleep 0.3s
 	else
 		echo -e "Libregeek Pacakge Signing Key [FAIL]. Adding now...\n"
 		sleep .5s
-		$scriptdir/utilities/gpg_import.sh 34C589A7 2> /dev/null
+		$scriptdir/utilities/gpg_import.sh 8106E72834C589A7
 	fi
 	
 	# Key Desc: Debian Multimedia
 	# Key ID: 65558117 
-	# Full Key ID: 5C808C2B65558117 
-	gpg_key_check=$(gpg --list-keys 65558117)
+	# Full Key ID: 5C808C2B65558117
+	gpg_key_check=$(gpg --list-keys 5C808C2B65558117)
 	if [[ "$gpg_key_check" != "" ]]; then
 		echo -e "Deb Mulitmedia [OK]\n"
 		sleep 0.3s
 	else
 		echo -e "Deb Mulitmedia [FAIL]. Adding now...\n"
 		sleep .5s
-		$scriptdir/utilities/gpg_import.sh 65558117 2> /dev/null
+		$scriptdir/utilities/gpg_import.sh 5C808C2B65558117
 	fi
 	
 }

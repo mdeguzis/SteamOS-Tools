56f0c2f7d1603bbbbedddacc2431b27913f327e2
mikeyd <mdeguzis@gmail.com>
2015-09-22 12:49:03 -0400

    update tests

--- utilities/test-suite.sh
+++ utilities/test-suite.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # -------------------------------------------------------------------------------
-# Author: 	Michael DeGuzis
-# Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
-# Scipt Name:	install-desktop-software.sh
-# Script Ver:	1.9.9.8
+# Author: 	    Michael DeGuzis
+# Git:	      	https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name: 	test-suite.sh
+# Script Ver:	  0.1.1
 # Description:	Runs some tests to make sure package installs and functions on a
 #               basic level work
 #
@@ -11,6 +11,80 @@
 # -------------------------------------------------------------------------------
 
 type="$1"
+
+show_summary()
+
+{
+  
+cat <<-EOF
+----------------------------------------
+Test suite $test Passed
+----------------------------------------
+EOF
+sleep 3s
+
+}
+
+show_failure()
+
+{
+  
+cat <<-EOF
+----------------------------------------
+Test suite $test FAILED!
+----------------------------------------
+EOF
+
+exit 1
+sleep 3s
+
+}
+
+reset_state()
+{
+  
+  echo -e "\n==> Resetting state"
+  sleep 2s
+  
+  # Remove packages
+  sudo apt-get get remove gedit
+  
+  # Purge archive package
+  sudo rm -f "/var/cache/apt/archive/gedit*"
+  
+}
+
+run_test()
+{
+  
+  if "${command}"; then
+  
+    # show summary
+    show_summary
+    
+  else
+    
+    # show failure
+    show_failure
+  
+  fi
+  
+}
+
+
+run_basic_tests()
+{
+  
+  # reset test package states
+  reset_state
+  
+  # desktop-software.sh tests
+  test="desktop-software.sh [Debian Package]"
+  pkg="gedit"
+  command="../desktop-software.sh install ${pkg}"
+  run_test
+  
+}
 
 main()
 {

cdb9358100db7c2fa8a266efa9186af4cd4b7049
mikeyd <mdeguzis@gmail.com>
2015-09-22 21:08:28 -0400

    change gpg to always import
    - this is due to /home staying on system restore
    - this messes up the keyring, since it is in gpg --list-keys, but not truly in the system
    - verifying this state is tough using an if statement, so just import them again.

--- add-debian-repos.sh
+++ add-debian-repos.sh
@@ -40,26 +40,12 @@ check_gpg()
 	# Key Desc: Libregeek Signing Key
 	# Key ID: 34C589A7
 	# Full Key ID: 8106E72834C589A7
-	if gpg --batch --quiet --edit-key 8106E72834C589A7 check clean save quit > /dev/null 2>&1; then
-		echo -e "Libregeek Pacakge Signing Key [OK]"
-		sleep 0.3s
-	else
-		echo -e "Libregeek Pacakge Signing Key [FAIL]. Adding now...\n"
-		sleep .5s
-		$scriptdir/utilities/gpg-import.sh 8106E72834C589A7 2> /dev/null
-	fi
+	$scriptdir/utilities/gpg-import.sh 8106E72834C589A7 2> /dev/null
 	
 	# Key Desc: Debian Multimedia
 	# Key ID: 65558117 
 	# Full Key ID: 5C808C2B65558117
-	if gpg --batch --quiet --edit-key 5C808C2B65558117 check clean save quit > /dev/null 2>&1; then
-		echo -e "Deb Mulitmedia [OK]\n"
-		sleep 0.3s
-	else
-		echo -e "Deb Mulitmedia [FAIL]. Adding now...\n"
-		sleep .5s
-		$scriptdir/utilities/gpg-import.sh 5C808C2B65558117 2> /dev/null
-	fi
+	$scriptdir/utilities/gpg-import.sh 5C808C2B65558117 2> /dev/null
 	
 }
 

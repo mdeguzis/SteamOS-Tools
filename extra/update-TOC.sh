f5b75d04e95377b337de0210add7d403191c4c6f
ProfessorKaos64 <mdeguzis@gmail.com>
2015-09-26 10:54:13 -0400

    update doctoc script

--- extra/update-TOC.sh
+++ extra/update-TOC.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+# ensure doctoc is install
+
+pkg_check=$(which doctoc)
+
+if [[ "$pkg_check" == "" ]]; then
+
+	sudo apt-get install npm
+	sudo npm install -g doctoc
+
+fi
+
+# set dir
 if [[ -d "$HOME/github_repos/SteamOS-Tools.wiki" ]]; then
 
 	dir="$HOME/github_repos/SteamOS-Tools.wiki"
 
 elif [[ -d "$HOME/SteamOS-Tools.wiki" ]]; then
 
+	dir="$HOME/SteamOS-Tools.wiki"
+
+else
+
+	# clone wiki to $HOME and set dir
+	cd
+	git clone https://github.com/ProfessorKaos64/SteamOS-Tools.wiki.git
 	dir="$HOME/SteamOS-Tools.wiki"
 
 fi
@@ -15,12 +34,12 @@ fi
 
 # update
 cd $dir
-git pull 
+git pull
 
 clear
 
 # show wiki dir
-ls
+ls $dir
 
 echo -e "\nUpdate which wiki page?\n"
 sleep 1s

e1e994be436290676d1c642d51a92d7e39ace152
mikeyd <mdeguzis@gmail.com>
2015-09-29 11:14:49 -0400

    Create build-steamos-xpad-dkms.sh

---
+++ utilities/build-scripts/build-steamos-xpad-dkms.sh
@@ -0,0 +1,250 @@
+#========================================================================
+# Build Script for steamos-xpad-dkms
+#======================================================================== 
+#
+# Author:  Michael DeGuzis, 
+# Date:    20150929
+# Version: 1.1
+# 
+# ========================================================================
+
+
+# define base version
+PRE=0
+BASE=1.1
+
+# define patch level
+PL=0
+
+# Define release
+REL="vivid"
+
+#define branch
+BRANCH=steamos-xpad-dkms
+
+#define upload target
+LAUNCHPAD_PPA="ppa:mdeguzis/steamos-tools"
+
+#define uploader for changelog
+uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
+
+#define manpage program author
+manpage_author="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
+
+#define package maintainer for dsc and control file 
+pkgmaintainer="SteamOS-Tools Team <mdeguzis@gmail.com>"
+
+
+clear
+echo "#####################################################################"
+echo "Building steamos-xpad-dkms (patch level $PL)"
+echo "#####################################################################"
+echo ""
+if [[ -n "$1" ]]; then
+
+  echo ""
+  echo "build target is $1"
+  echo ""
+
+else
+  echo ""
+  echo "build target is source"
+  echo ""
+fi
+
+sleep 2s
+
+# Fetch build pkgs
+if [[ -n "$2" ]]; then
+
+  echo ""
+  echo "##########################################"
+  echo "Fetching necessary packages for build"
+  echo "##########################################"
+  echo ""
+
+  #apt-get install packages
+  sudo apt-get install -y build-essential fakeroot devscripts automake autoconf autotools-dev
+
+  #get build dependencies
+  sudo apt-get -y install debhelper cmake gcc
+
+else
+  echo ""
+  echo "skipping installation of build packages, use arbitrary second argument to get those packages"
+  echo ""
+fi
+
+echo ""
+echo "##########################################"
+echo "Setup build directory"
+echo "##########################################"
+echo ""
+echo "~/pkg-build-tmp/steamos-xpad-dkms"
+# start in $HOME
+cd
+
+# remove old build directory
+rm -rf ~/pkg-build-tmp/steamos-xpad-dkms
+
+#create build directory
+mkdir -p ~/pkg-build-tmp/steamos-xpad-dkms
+
+#change to build directory
+cd ~/pkg-build-tmp/steamos-xpad-dkms
+
+echo ""
+echo "##########################################"
+echo "Setup package base files"
+echo "##########################################"
+
+echo "dsc file"
+cp ~/SteamOS-Tools/utilities/build-scripts/steamos-xpad-dkms/steamos-xpad-dkms.dsc steamos-xpad-dkms-$PRE:$BASE.$PL$REL.dsc
+sed -i "s|version_placeholder|$PRE:$BASE.$PL$REL|g" "steamos-xpad-dkms-$PRE:$BASE.$PL$REL.dsc"
+sed -i "s|pkgmaintainer|$pkgmaintainer|g" "steamos-xpad-dkms-$PRE:$BASE.$PL$REL.dsc"
+
+echo "original tarball"
+git clone -b steamos-xpad-dkms https://github.com/ProfessorKaos64/SteamOS-Tools
+
+# sanity check
+file steamos-xpad-dkms/
+
+if [ $? -eq 0 ]; then  
+    echo "successfully cloned/copied"
+else  
+    echo "git clone/copy failed, aborting"
+    exit
+fi 
+
+# change to source folder
+cd steamos-xpad-dkms
+git pull
+git checkout $BRANCH
+# remove git files
+rm -rf .git .gitignore .hgeol .hgignore
+
+# Create archive
+cd ..
+tar cfj steamos-xpad-dkms.orig.tar.bz2 steamos-xpad-dkms
+mv steamos-xpad-dkms.orig.tar.bz2 steamos-xpad-dkms_$BASE.$PL$REL.orig.tar.bz2
+
+echo ""
+echo "##########################################"
+echo "Unpacking debian files"
+echo "##########################################"
+echo ""
+
+# enter github repository
+cd steamos-xpad-dkms
+# enter debian build folder
+cd steamos-xpad-dkms
+
+echo "changelog"
+cp ~/SteamOS-Tools/utilities/build-scripts/steamos-xpad-dkms/debian/changelog debian/
+sed -i "s|version_placeholder|$PRE:$BASE.$PL$REL|g" debian/changelog
+sed -i "s|uploader|$uploader|g" debian/changelog
+
+echo "copyright"
+cp ~/SteamOS-Tools/utilities/build-scripts/steamos-xpad-dkms/debian/copyright debian/
+sed -i "s|pkgmaintainer|$pkgmaintainer|g" debian/copyright
+
+echo "control"
+cp ~/SteamOS-Tools/utilities/build-scripts/steamos-xpad-dkms/debian/control debian/
+sed -i "s|pkgmaintainer|$pkgmaintainer|g" debian/control
+
+echo "rules"
+cp ~/SteamOS-Tools/utilities/build-scripts/steamos-xpad-dkms/debian/rules debian/
+
+echo "format"
+cp ~/SteamOS-Tools/utilities/build-scripts/steamos-xpad-dkms/debian/source/format debian/source/
+
+echo "manpage"
+cp ~/SteamOS-Tools/utilities/build-scripts/steamos-xpad-dkms/debian/steamos-xpad-dkms.7 debian/
+cp ~/SteamOS-Tools/utilities/build-scripts/steamos-xpad-dkms/debian/steamos-xpad-dkms.manpages debian/
+sed -i "s|author_temp|$manpage_author|g" debian/steamos-xpad-dkms.7
+
+if [[ -n "$1" ]]; then
+  arg0=$1
+else
+  # set up default
+  arg0=source
+fi
+
+case "$arg0" in
+  compile)
+    echo ""
+    echo "##########################################"
+    echo "Building binary package now"
+    echo "##########################################"
+    echo ""
+
+    #build binary package
+    debuild -us -uc
+
+    if [ $? -eq 0 ]; then  
+        echo ""
+        echo "##########################################"
+        echo "Building finished"
+        echo "##########################################"
+        echo ""
+        ls -lah ~/pkg-build-tmp/steamos-xpad-dkms
+         exit 0
+    else  
+        echo "debuild failed to generate the binary package, aborting"
+        exit 1
+    fi 
+    ;;
+  source)
+    #get secret key
+    gpgkey=`gpg --list-secret-keys|grep "sec   "|cut -f 2 -d '/'|cut -f 1 -d ' '`
+
+    if [[ -n "$gpgkey" ]]; then
+
+      echo ""
+      echo "##########################################"
+      echo "Building source package"
+      echo "##########################################"
+      echo ""
+      echo ""
+      echo ""
+      echo "****** please copy your gpg passphrase into the clipboard ******"
+      echo ""
+      sleep 10
+
+      debuild -S -sa -k$gpgkey
+
+      if [ $? -eq 0 ]; then
+        echo ""
+        echo ""
+        ls -lah ~/pkg-build-tmp/steamos-xpad-dkms
+        echo ""
+        echo ""
+        echo "you can upload the package with dput ppa:mdeguzis/retrorig-es ~/pkg-build-tmp/steamos-xpad-dkms/steamos-xpad-dkms/steamos-xpad-dkms_$BASE.$PL$REL""_source.changes"
+        echo "all good"
+        echo ""
+        echo ""
+
+        while true; do
+            read -p "Do you wish to upload the source package?    " yn
+            case $yn in
+                [Yy]* ) dput ppa:mdeguzis/steamos-tools ~/pkg-build-tmp/steamos-xpad-dkms/steamos-xpad-dkms/steamos-xpad-dkms_$BASE.$PL$REL""_source.changes; break;;
+                [Nn]* ) break;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+
+        exit 0
+      else	
+        echo "debuild failed to generate the source package, aborting"
+        exit 1
+      fi
+    else
+      echo "secret key not found, aborting"
+      exit 1
+    fi
+    ;;
+esac
+
+
+
+

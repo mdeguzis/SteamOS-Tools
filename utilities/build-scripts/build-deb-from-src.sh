30562e2332911ee1303d7ee86ae65ecaa88a3c1c
mikeyd <mdeguzis@gmail.com>
2015-10-06 18:37:51 -0400

    needed --force-yes for pkg auto-installs

--- utilities/build-scripts/build-deb-from-src.sh
+++ utilities/build-scripts/build-deb-from-src.sh
@@ -50,7 +50,7 @@ install_prereqs()
 	echo -e "==> Assessing prerequisites for building...\n"
 	sleep 1s
 	# install needed packages
-	sudo apt-get install git devscripts build-essential checkinstall \
+	sudo apt-get install -y --force-yes git devscripts build-essential checkinstall \
 	debian-keyring debian-archive-keyring cmake g++ g++-multilib \
 	libqt4-dev libqt4-dev libxi-dev libxtst-dev libX11-dev bc libsdl2-dev \
 	gcc gcc-multilib

8e459896934dce9655da355b97200f5c12e99304
mikeyd <mdeguzis@gmail.com>
2015-10-06 13:31:00 -0400

    var change

--- utilities/testing/steamos-chroot-post-install.sh
+++ utilities/testing/steamos-chroot-post-install.sh
@@ -34,7 +34,7 @@ policy="./usr/sbin/policy-rc.d"
 type="tmp_type"
 stock_opt="tmp_stock"
 release="tmp_release"
-target="${type}-${release}"
+target="tmp_target"
 	
 # pass to ensure we are in the chroot 
 # temp test for chroot (output should be something other than 2)

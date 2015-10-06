824f2890c2bba5bd5cbad3a01077da02bdd356a7
mikeyd <mdeguzis@gmail.com>
2015-10-06 07:51:49 -0400

    update checkinstall presets

--- utilities/build-scripts/build-deb-from-src.sh
+++ utilities/build-scripts/build-deb-from-src.sh
@@ -193,7 +193,8 @@ main()
 	sleep 2s
 	
 	# build deb package
-	sudo checkinstall
+	sudo checkinstall --fstrans="no" --backup="no" \
+	--pkgversion="$(date +%Y%m%d)+git" --deldoc="yes" --exclude="/home"
 
 	# Alternate method
 	# dpkg-buildpackage -us -uc -nc
@@ -239,29 +240,6 @@ main()
 	
 	echo -e "Showing contents of: $git_dir: \n"
 	ls "$git_dir"
-
-	echo -e "\n==> Would you like to trim tar.gz, dsc files, and folders for uploading? [y/n]"
-	sleep 0.5s
-	# capture command
-	read -ep "Choice: " trim_choice
-	
-	if [[ "$trim_choice" == "y" ]]; then
-		
-		# cut files so we just have our deb pkg
-		sudo rm -f $git_dir/*.tar.gz
-		sudo rm -f $git_dir/*.dsc
-		sudo rm -f $git_dir/*.changes
-		sudo rm -f $git_dir/*-dbg
-		sudo rm -f $git_dir/*-dev
-		sudo rm -f $git_dirs/*-compat
-		
-		# remove source directory that was made
-		find $build_dir -mindepth 1 -maxdepth 1 -type d -exec rm -r {} \;
-		
-	elif [[ "$trim_choice" == "n" ]]; then
-	
-		echo -e "File trim not requested"
-	fi
 
 	echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
 	sleep 0.5s

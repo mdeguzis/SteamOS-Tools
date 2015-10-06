4a5f17bfea67911b2a337e812fa6d09987c92b7d
mikeyd <mdeguzis@gmail.com>
2015-10-06 10:46:00 -0400

    Update build-simplescreenrecorder.sh

--- utilities/build-scripts/build-simplescreenrecorder.sh
+++ utilities/build-scripts/build-simplescreenrecorder.sh
@@ -45,6 +45,16 @@ install_prereqs()
 	libavcodec-dev libavutil-dev libswscale-dev libasound2-dev libpulse-dev libjack-jackd2-dev \
 	libgl1-mesa-dev libglu1-mesa-dev libx11-dev libxfixes-dev libxext-dev libxi-dev g++-multilib \
 	libx11-6 libxext6 libxext-dev libxfixes3 libxfixes3:i386 libglu1-mesa:i386 ffmpeg
+	
+	# some libraries are not foudn with the current build script
+	# Per the readme file, symlink these
+	cd /usr/lib/i386-linux-gnu
+	sudo ln -s libGL.so.1 libGL.so
+	sudo ln -s libGLU.so.1 libGLU.so
+	sudo ln -s libX11.so.6 libX11.so
+	sudo ln -s libXext.so.6 libXext.so
+	sudo ln -s libXfixes.so.3 libXfixes.so
+	sudo ldconfig
 
 }
 
@@ -87,7 +97,7 @@ main()
 		
 	else 
 	
-		echo -e "\n==ERROR==\n${pkgname}build FAILED. Exiting in 15 seconds"
+		echo -e "\n==ERROR==\n${pkgname} build FAILED. Exiting in 15 seconds"
 		sleep 15s
 		exit 1
 		

f2b8669b5d52a17ccd307a3f7414c84febc107bf
mikeyd <mdeguzis@gmail.com>
2015-10-06 20:37:57 -0400

    incorrect git source

--- utilities/build-scripts/build-speetest-cli.sh
+++ utilities/build-scripts/build-speetest-cli.sh
@@ -18,7 +18,7 @@ time_stamp_start=(`date +"%T"`)
 src_cmd=""
 
 # upstream URL
-git_url="https://github.com/MaartenBaert/ssr"
+git_url="https://github.com/sivel/speedtest-cli"
 
 # package vars
 pkgname="speedtest-cli"
@@ -65,7 +65,7 @@ main()
 	
 	# Clone upstream source code
 	
-	echo -e "==> Obtaining upstream source code\n"
+	echo -e "\n==> Obtaining upstream source code\n"
 	
 	git clone "$git_url" "$git_dir"
 	

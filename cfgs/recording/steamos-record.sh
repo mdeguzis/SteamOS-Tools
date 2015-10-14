18064ce9375c2eaa5a29114e772d3fa14741b45a
mikeyd <mdeguzis@gmail.com>
2015-10-14 06:42:51 -0400

    var fixes

--- cfgs/recording/steamos-record.sh
+++ cfgs/recording/steamos-record.sh
@@ -5,7 +5,7 @@ rm -f /tmp/pulse.wav
 
 #set variables
 RECORDER="avconv"
-DIR="~/Videos"
+DIR="$HOME/Videos"
 FRAMERATE="30"
 STARTNAME="recording"
 FORMAT="avi"
@@ -18,10 +18,10 @@ exec | tee ${LOG}
 # if a recording is already running, kill it and stop the script
 if [[ ! -z $(ps aux|awk '{print $11}'|grep ${RECORDER}) ]]; then
 
-	killall ${RECORDER}
-	echo "${RECORDER} closed by recording-start.sh."2>>${LOG}
-	exit 0
-	
+        killall ${RECORDER}
+        echo "${RECORDER} closed by recording-start.sh."2>>${LOG}
+        exit 0
+
 fi
 
 #grab resolution
@@ -30,18 +30,18 @@ RES=$(xdpyinfo|grep dimensions|awk '{print $2}')
 # make recording directories if they don't exist yet
 if [[ ! -d ${DIR} ]]; then
 
-	mkdir -p ${DIR}
-	
+        mkdir -p ${DIR}
+
 fi
 
 # set name of the recording
 DATE=$(date +"_%Y%m%d")
 NUMBER=1
-while [ -f $DIR/${STARTNAME}${NUMBER}_${DATE}.$FORMAT ]
+while [ -f $DIR/${STARTNAME}${NUMBER}${DATE}.$FORMAT ]
 do
         NUMBER=$((${NUMBER}+1))
 done
-NAME=${STARTNAME}${NUMBER}_${DATE}
+NAME=${STARTNAME}${NUMBER}${DATE}
 
 # start the recording
 ${RECORDER} -f pulse -i default /tmp/pulse.wav -f x11grab -r ${FRAMERATE} -s ${RES} -i ${DISPLAY} -acodec pcm_s16le -vcodec libx264 -preset ultrafast -crf 0 -threads 0 ${DIR}/${NAME}.avi

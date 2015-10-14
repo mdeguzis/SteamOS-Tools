2a638f4f0811391ccd572311fbcd6b0ab876a10e
mikeyd <mdeguzis@gmail.com>
2015-10-14 06:37:58 -0400

    fix up some vars

--- cfgs/recording/steamos-record.sh
+++ cfgs/recording/steamos-record.sh
@@ -4,12 +4,12 @@
 rm -f /tmp/pulse.wav
 
 #set variables
-RECORDER=avconv
-DIR=~/Videos
-FRAMERATE=30
-STARTNAME=recording
-FORMAT=avi
-LOG=/tmp/dumps/${RECORDER}-recording.log
+RECORDER="avconv"
+DIR="~/Videos"
+FRAMERATE="30"
+STARTNAME="recording"
+FORMAT="avi"
+LOG="/tmp/dumps/${RECORDER}-recording.log"
 export DISPLAY=:0.0
 
 # log errors
@@ -30,7 +30,7 @@ RES=$(xdpyinfo|grep dimensions|awk '{print $2}')
 # make recording directories if they don't exist yet
 if [[ ! -d ${DIR} ]]; then
 
-	mkdir $DIR
+	mkdir ${DIR}
 	
 fi
 
@@ -44,7 +44,7 @@ done
 NAME=${STARTNAME}${NUMBER}_${DATE}
 
 # start the recording
-avconv -f pulse -i default /tmp/pulse.wav -f x11grab -r ${FRAMERATE} -s $RES -i $DISPLAY -acodec pcm_s16le -vcodec libx264 -preset ultrafast -crf 0 -threads 0 $DIR/$NAME.avi
+${RECORDER} -f pulse -i default /tmp/pulse.wav -f x11grab -r ${FRAMERATE} -s ${RES} -i ${DISPLAY} -acodec pcm_s16le -vcodec libx264 -preset ultrafast -crf 0 -threads 0 ${DIR}/${NAME}.avi
 
 # In case the recording does finish/crash
 rm /tmp/pulse.wav

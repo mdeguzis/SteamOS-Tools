fb6cd850930c1657cd49fe99bb93ff045aae788c
mikeyd <mdeguzis@gmail.com>
2015-10-13 20:50:41 -0400

    update to kill previous recording

--- cfgs/recording/recording-start.sh
+++ cfgs/recording/recording-start.sh
@@ -5,7 +5,19 @@ DIR=~/Videos
 FRAMERATE=30
 STARTNAME=recording
 FORMAT=avi
+LOG=/tmp/dumps/${RECORDER}-recording.log
 export DISPLAY=:0.0
+
+# log errors
+exec 2>${LOG}
+
+# if a recording is already running, kill it and stop the script
+if [[ ! -z $(ps aux|awk '{print $11}'|grep ${RECORDER}) ]]
+then
+	killall ${RECORDER}
+	echo "${RECORDER} closed by recording-start.sh."2>>${LOG}
+	exit 0
+fi
 
 #grab resolution
 RES=$(xdpyinfo|grep dimensions|awk '{print $2}')
@@ -17,13 +29,12 @@ mkdir $DIR
 fi
 
 # set name of the recording
-DATE=$(date +"_%Y%m%d")
 NUMBER=1
 while [ -f $DIR/$STARTNAME$NUMBER.$FORMAT ]
 do
-NUMBER=$(($NUMBER+1))
+	NUMBER=$(($NUMBER+1))
 done
-NAME=$STARTNAME$NUMBER$DATE 
+NAME=$STARTNAME$NUMBER
 
 # start the recording
 avconv -f pulse -i default /tmp/pulse.wav -f x11grab -r ${FRAMERATE} -s $RES -i $DISPLAY -acodec pcm_s16le -vcodec libx264 -preset ultrafast -crf 0 -threads 0 $DIR/$NAME.avi

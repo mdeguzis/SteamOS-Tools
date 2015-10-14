a40cf32a9ff5e0d872835a458875ae29f226a71c
mikeyd <mdeguzis@gmail.com>
2015-10-13 21:09:53 -0400

    fix file naming, syntax

--- cfgs/recording/steamos-record.sh
+++ cfgs/recording/steamos-record.sh
@@ -15,30 +15,32 @@ export DISPLAY=:0.0
 exec | tee ${LOG}
 
 # if a recording is already running, kill it and stop the script
-if [[ ! -z $(ps aux|awk '{print $11}'|grep ${RECORDER}) ]]
-then
+if [[ ! -z $(ps aux|awk '{print $11}'|grep ${RECORDER}) ]]; then
+
 	killall ${RECORDER}
 	echo "${RECORDER} closed by recording-start.sh."2>>${LOG}
 	exit 0
+	
 fi
 
 #grab resolution
 RES=$(xdpyinfo|grep dimensions|awk '{print $2}')
 
 # make recording directories if they don't exist yet
-if [ ! -d ${DIR} ]
-then
-mkdir $DIR
+if [[ ! -d ${DIR} ]]; then
+
+	mkdir $DIR
+	
 fi
 
 # set name of the recording
 DATE=$(date +"_%Y%m%d")
 NUMBER=1
-while [ -f $DIR/$STARTNAME$NUMBER.$FORMAT ]
+while [ -f $DIR/${STARTNAME}${NUMBER}_${DATE}.$FORMAT ]
 do
-	NUMBER=$(($NUMBER+1))
+        NUMBER=$((${NUMBER}+1))
 done
-NAME=$STARTNAME$NUMBER_$date
+NAME=${STARTNAME}${NUMBER}_${DATE}
 
 # start the recording
 avconv -f pulse -i default /tmp/pulse.wav -f x11grab -r ${FRAMERATE} -s $RES -i $DISPLAY -acodec pcm_s16le -vcodec libx264 -preset ultrafast -crf 0 -threads 0 $DIR/$NAME.avi

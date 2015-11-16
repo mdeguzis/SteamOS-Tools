a90e0527a538affa220645df8884d52bcf309af5
ProfessorKaos64 <mdeguzis@gmail.com>
2015-11-15 21:49:38 -0500

    remove old build scripts

--- utilities/build-scripts/kodi/pvr.mythtv-Isengard/src/cppmyth.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "cppmyth/MythChannel.h"
-#include "cppmyth/MythEPGInfo.h"
-#include "cppmyth/MythProgramInfo.h"
-#include "cppmyth/MythRecordingRule.h"
-#include "cppmyth/MythScheduleManager.h"

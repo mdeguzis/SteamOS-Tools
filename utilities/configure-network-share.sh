62f3a6573e53fdcfc11313554e0c6a47f0d5f836
ProfessorKaos64 <mdeguzis@gmail.com>
2015-12-16 19:38:21 -0500

    fix text formatting

--- utilities/configure-network-share.sh
+++ utilities/configure-network-share.sh
@@ -13,89 +13,88 @@ scriptdir=$(pwd)
 install_prereqs()
 {
 
-  # Install needed software
-  sudo apt-get install -y --force-yes samba smbclient
+	# Install needed software
+	sudo apt-get install -y --force-yes samba smbclient
 
 }
 
 main_menu()
 {
-  while [[ "$configure_choice" != "4" || "$configure_choice" != "done" ]];
-  		do	
-  		
-  		cat<<- EOF 
-  		#############################################################
-  		What do you wish to do?
-  		#############################################################
-  		
-  		(1) Create publich network share (no password)
-  		(2) Connect existing share permanently
-  		(3) Connect existing share temporarily
-  		(4) Exit
+	while [[ "$configure_choice" != "4" || "$configure_choice" != "done" ]];
+ 	do
 
-      EOF
-      
-  		# the prompt sometimes likes to jump above sleep
-  		sleep 0.5s
-  	
-  		read -erp "Choice: " configure_choice
-  	
-  		case "$configure_choice" in
-  	
-  			1)
-  			share_type="public"
-  			create_samba_share
-  			;;
-  	
-  			2)
-  			# attach_share_perm
-  			;;
-  			 
-  			3)
-  			# attach_share_temp
-  			;;
+		cat<<- EOF
+		#############################################################
+  		What do you wish to do?
+	  	#############################################################
+		(1) Create publich network share (no password)
+		(2) Connect existing share permanently
+		(3) Connect existing share temporarily
+		(4) Exit
+
+		EOF
+
+		# the prompt sometimes likes to jump above sleep
+		sleep 0.5s
+
+		read -erp "Choice: " configure_choice
+
+		case "$configure_choice" in
+
+		1)
+		share_type="public"
+		create_samba_share
+		;;
+
+		2)
+	  	# attach_share_perm
+  		;;
+
+		3)
+		# attach_share_temp
+		;;
 
   		esac
-
-  done
+	done
 
 }
 
 create_samba_share()
 {
-  #################################################
-  # Samba Shares
-  #################################################
-  # https://wiki.archlinux.org/index.php/Samba/Tips_and_tricks#Share_files_without_a_username_and_password
-  
-  echo -e "\n==> Configuring samba shares for remote access"
-  
-  # remove old shares
-  
-  # For saftely, a start/end method is used here to only kill the lines we added
-  # This will only remove the tagged block of text
-  # This keeps the share itself updated from whatever the repo is, of course provided
-  # the tags are kept (note to self)
-  
-  # Name the share
-  read -erp "Please name your share: " MYSHARE
-  
-  # See: http://serverfault.com/a/137848
-  # Remove our previous entry added, to update with corrections or changes
-  sudo sed -i '\:# START $MYSHARE shares:,\:# END $MYSHARE shares:d' "/etc/samba/smb.conf"
-  
-  # append config for shares
-  if [[ "$share_type" == "public" ]]; then
-  
-    sudo bash -c 'cat "../cfgs/samba/generic-share-public.cfg" >> "/etc/samba/smb.conf"'
-    
-  fi
-  
-  # restart services smbd and nmbd, not samba
-  # Debian bug: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=769714
-  sudo systemctl restart smbd
-  sudo systemctl restart nmbd
-  
+
+	#################################################
+	# Samba Shares
+	#################################################
+	# https://wiki.archlinux.org/index.php/Samba/Tips_and_tricks#Share_files_without_a_username_and_password
+
+	echo -e "\n==> Configuring samba shares for remote access"
+
+	# remove old shares
+
+	# For saftely, a start/end method is used here to only kill the lines we added
+	# This will only remove the tagged block of text
+	# This keeps the share itself updated from whatever the repo is, of course provided
+	# the tags are kept (note to self)
+
+	# Name the share
+	read -erp "Please name your share: " MYSHARE
+
+	# See: http://serverfault.com/a/137848
+	# Remove our previous entry added, to update with corrections or changes
+	sudo sed -i '\:# START $MYSHARE shares:,\:# END $MYSHARE shares:d' "/etc/samba/smb.conf"
+
+	# append config for shares
+	if [[ "$share_type" == "public" ]]; then
+
+		sudo bash -c 'cat "../cfgs/samba/generic-share-public.cfg" >> "/etc/samba/smb.conf"'
+
+	fi
+
+	# restart services smbd and nmbd, not samba
+	# Debian bug: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=769714
+	sudo systemctl restart smbd
+	sudo systemctl restart nmbd
+
 }
 
 # Install needed software

69cdf18993842cbe412d78c47437e42dfa6933d8
mikeyd <mdeguzis@gmail.com>
2015-12-30 09:47:38 -0500

    Create setup-wine.game.sh

---
+++ utilities/wine/setup-wine.game.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -------------------------------------------------------------------------------
+# Author:    	  Michael DeGuzis
+# Git:	    	  https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	  build-cmake.sh
+# Script Ver:	  0.7.9
+# Description:	  Adds a given game to Steam running under Wine/POL
+# See:		  
+# Usage:	./setup-wine.game.sh
+# -------------------------------------------------------------------------------
+
+# See TODO.md for now

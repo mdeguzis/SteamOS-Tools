ed72733eb46feb1935d6ff6bdff671d3fbafd9b8
professorkaos64 <mdeguzis@gmail.com>
2016-09-28 13:28:39 -0700

    remove api/ver for steam, seems to be removed?

--- utilities/steamos-stats.sh
+++ utilities/steamos-stats.sh
@@ -10,7 +10,7 @@
 # Usage:		./steamos-stats.sh -gpu [gfx driver] -appid [APPID]
 #			The cmd 'steamos-stats' can also be used post-install
 #
-# Warning:		You MUST have the Debian repos added properly for 
+# Warning:		You MUST have the Debian repos added properly for
 #			Installation of the pre-requisite packages.
 # TODO:			Add AMD GPU support
 # -----------------------------------------------------------------------------
@@ -25,11 +25,11 @@ funct_set_main_vars()
 	APPID_ENABLE="False"
 	APPID="0"
 	kernel_ver=$(uname -r)
-	# set default for now 
+	# set default for now
 	# Valve's installer will use proprietary drivers, if available
 	active_driver="nvidia"
 	supported_gpu="yes"
-	
+
 	# set default scriptdir
 	scriptdir="${PWD}"
 }
@@ -37,9 +37,9 @@ funct_set_main_vars()
 funct_pre_req_checks()
 {
 	# From user input (until auto detection is figured out), set
-	# the gpu driver on the first argument. 
+	# the gpu driver on the first argument.
 	# "Unsupported" may just mean the driver has not been tested yet
-	
+
 	# valid driver values: nvidia, intel, fglrx, nouveau, radeon
 	if [[ "$1" == "-driver" ]]; then
 		if [[ "$2" == "nvidia" ]]; then
@@ -47,7 +47,7 @@ funct_pre_req_checks()
 		    	supported_gpu="yes"
 		elif [[ "$2" == "nouveau" ]]; then
 		    	active_driver="nouveau"
-			supported_gpu="no"	
+			supported_gpu="no"
 		elif [[ "$2" == "fglrx" ]]; then
 		    	active_driver="fglrx"
 			supported_gpu="no"
@@ -59,12 +59,12 @@ funct_pre_req_checks()
 			supported_gpu="no"
 		fi
 	fi
-	
+
 	if [[ "$3" == "-appid" ]]; then
 		echo "appid detected"
 		APPID=$(echo $4)
 	fi
-	
+
 	# GPU testing ONLY!
 	#clear
 	#echo "Enabled options:"
@@ -72,22 +72,22 @@ funct_pre_req_checks()
 	#echo "APP ID: $APPID"
 	#echo "Kernel Ver: $kernelver"
 	#echo "Active GPU $active_gpu"
-	
+
 	clear
 	####################################################################
 	# Check for packages
 	####################################################################
-	
+
 	echo "#####################################################"
 	echo "Package pre-req checks"
 	echo "#####################################################"
-	
-	# set external Deb repo required flag 
-	export deb_repo_name="jessie.list" 
-	export deb_repo_req="yes" 
-	# Eval requirements 
-	"${scriptdir}/check_repo_req.sh" 
-	
+
+	# set external Deb repo required flag
+	export deb_repo_name="jessie.list"
+	export deb_repo_req="yes"
+	# Eval requirements
+	"${scriptdir}/check_repo_req.sh"
+
 	#####################################################"
 	# SteamCMD
 	#####################################################"
@@ -107,7 +107,7 @@ funct_pre_req_checks()
 	else
 		echo -e "\nChecking for lib32gcc1: [Ok]"
 	fi
-	
+
 	# check for SteamCMD's existance in /home/desktop
 	if [[ ! -f "/home/desktop/steamcmd/steamcmd.sh" ]]; then
 		echo -e "\nsteamcmd not found\n"
@@ -136,16 +136,16 @@ funct_pre_req_checks()
 	else
 		echo "Checking for 'steamcmd' [Ok]"
 	fi
-	
+
 	#####################################################"
 	# VaporOS bindings (controller shortcuts)
 	#####################################################"
 	# FPS + more binds from VaporOS 2
 	# For bindings, see: /etc/actkbd-steamos-controller.conf
-	
+
 	echo -e "\n==> VaporOS Xbox 360 bindings"
 	sleep 1s
-	
+
 	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' vaporos-binds-xbox360 | grep "install ok installed")
 	if [ "" == "$PKG_OK" ]; then
 		echo -e "vaporos-binds-xbox360 not found. Setting up vaporos-binds-xbox360 now...\n"
@@ -190,7 +190,7 @@ funct_pre_req_checks()
 		if [[ -d "/etc/apt/preferences" ]]; then
 			# backup preferences file
 			sudo mv "/etc/apt/preferences" "/etc/apt/preferences.bak"
-		fi 
+		fi
 
 		sudo apt-get update -y
 		sudo apt-get install -y --force-yes steamos-dev  git ca-certificates \
@@ -213,7 +213,7 @@ funct_pre_req_checks()
 			# restore preferences file
 			sudo mv "/etc/apt/preferences.bak" "/etc/apt/preferences"
 		fi
-	
+
 		# Update
 		sudo apt-get update
 		cd
@@ -232,7 +232,7 @@ funct_pre_req_checks()
 	fi
 
 	#####################################################"
-	# Other core utilties from official repos 
+	# Other core utilties from official repos
 	#####################################################"
 
 	echo -e "\n==> Checking for other core utilties\n"
@@ -240,7 +240,7 @@ funct_pre_req_checks()
 
 	CORE_PKGS="lm-sensors sysstat git nvidia-smi openssh-server"
 
-	for PKG in ${CORE_PKGS}; 
+	for PKG in ${CORE_PKGS};
 	do
 
 		if [[ $(dpkg-query -s ${PKG}) == "" ]]; then
@@ -253,17 +253,13 @@ funct_pre_req_checks()
 		else
 			echo "package ${PKG} [OK]"
 		fi
-	
+
 	done
 
 	# notify user if GPU is supported by utility
 	echo -e "\n==> GPU status"
 	echo -e "\nSupported GPU: $supported_gpu"
 
-	# TESTING ONLY - pause for pkg check testing
-	# echo -e "\nPausing for dramatic effect ^_^ "
-	# sleep 10s
-	
 	####################################################################
 	# voglperf
 	####################################################################
@@ -271,7 +267,7 @@ funct_pre_req_checks()
 	# Full AppID game list: http://steamdb.info/linux/
 	# Currently, still easier to use Shark's VaporOS package, which adds
 	# easy gamepad toggles for an FPS overlay
-	
+
 	# Accept game ID argument. If found, turn APPID=True
 	if [[ "$APPID_ENABLE" == "true" ]]; then
 	   echo ""
@@ -280,11 +276,11 @@ funct_pre_req_checks()
 	   # Volgperf integration is disabled for now
 	   # echo -ne 'showfps on\n' |  echo -ne 'game start $APPID \n' | sudo -u steam /home/desktop/voglperf/bin/voglperfrun64
 	fi
-	
+
 	####################################################################
 	# Post install
 	####################################################################
-	
+
 	# copy script to /usr/bin for easy use
 	sudo cp "${scriptdir}/steamos-stats.sh" "/usr/bin/steamos-stats"
 	sudo chmod +x "/usr/bin/steamos-stats"
@@ -295,42 +291,44 @@ funct_main_loop()
 	####################################################################
 	# Start Loop
 	####################################################################
-	
+
 	# Loop until a key is pressed [IN TESTING]
 	# Credit to http://stackoverflow.com/users/111461/sam-hocevar
 	# See: http://stackoverflow.com/a/5297780
-	if [ -t 0 ]; then 
-		stty -echo -icanon -icrnl time 0 min 0; 
+	if [ -t 0 ]; then
+		stty -echo -icanon -icrnl time 0 min 0;
 	fi
-		
+
 	count=0
 	keypress=''
 
 	while [ "x$keypress" = "x" ]; do
-		
+
 		let count+=1
 		# testing only
 	  	# echo -ne $count'\r'
 		keypress="`cat -v`"
-	
+
 		########################################
 		# Set VARS
 		########################################
 		CEL=$(echo $'\xc2\xb0'C)
-	
+
 		CPU=$(less /proc/cpuinfo | grep -m 1 "model name" | cut -c 14-70)
 		CPU_TEMPS=$(sensors | grep -E '(Core|Physical)'| iconv -f ISO-8859-1 -t UTF-8)
-	
+
 		#also see: xxd, iconv
 		CPU_LOAD=$(iostat | cut -f 2 | grep -A 1 "avg-cpu")
 		MEM_LOAD=$(free -m | grep -E '(total|Mem|Swap)' |  cut -c 1-7,13-18,23-29,34-40,43-51,53-62,65-73)
-		
+
 		# Steam-specific
-		# There is a bug in the current steamcmd version that outputs a 
+		# There is a bug in the current steamcmd version that outputs a
 		# Danish "o" in "version"
+
+		# These don't seem to resolve anymore, can't find this command in the find command
 		steam_ver=$(/home/desktop/steamcmd/steamcmd.sh "+versi$(echo -e '\xc3\xb8')n" +quit | grep "package" | cut -c 25-35)
 		steam_api=$(/home/desktop/steamcmd/steamcmd.sh "+versi$(echo -e '\xc3\xb8')n" +quit | grep -E "^Steam API\:" | cut -c 12-15)
-	
+
 		# Determine which GPU chipset we are dealing with
 		# Currently, Nvidia is only supported
 		# Other values: intel, fglrx, nouveau, radeon
@@ -340,42 +338,42 @@ funct_main_loop()
 			GPU_DRIVER=$(nvidia-smi -a | grep -E 'Driver Version' | cut -c 39-100)
 			GPU_TEMP=$(nvidia-smi -a | grep -E 'Current Temp' | cut -c 39-40 | sed "s|$|$CEL|g")
 			GPU_FAN=$(nvidia-smi -a | grep -E 'Fan Speed' | cut -c 39-45 | sed "s| %|%|g")
-			
+
 		elif [[ "$active_driver" == "nouveau" ]]; then
 			GPU="          [temporarily disabled]"
 			GPU_DRIVER="[temporarily disabled]"
 			GPU_TEMP="          [temporarily disabled]"
 			GPU_FAN="     [temporarily disabled]"
-	
+
 		elif [[ "$active_driver" == "fglrx" ]]; then
 			GPU="          [temporarily disabled]"
 			GPU_DRIVER="[temporarily disabled]"
 			GPU_TEMP="          [temporarily disabled]"
 			GPU_FAN="     [temporarily disabled]"
-	
+
 		elif [[ "$active_driver" == "intel" ]]; then
 			GPU="          [temporarily disabled]"
 			GPU_DRIVER="[temporarily disabled]"
 			GPU_TEMP="          [temporarily disabled]"
 			GPU_FAN="     [temporarily disabled]"
-			
+
 		elif [[ "$active_driver" == "radeon" ]]; then
 			GPU="          [temporarily disabled]"
 			GPU_DRIVER="[temporarily disabled]"
 			GPU_TEMP="          [temporarily disabled]"
 			GPU_FAN="     [temporarily disabled]"
-			
+
 		elif [[ "$active_driver" == "intel" ]]; then
 			GPU="          [temporarily disabled]"
 			GPU_DRIVER="[temporarily disabled]"
 			GPU_TEMP="          [temporarily disabled]"
 			GPU_FAN="     [temporarily disabled]"
-	
+
 		else
 			#nothing to see here for now
 			echo "" > /dev/null
 		fi
-	
+
 		clear
 		echo "###########################################################"
 		echo "Monitoring system statistics... |  Press any key to quit  #"
@@ -393,11 +391,11 @@ funct_main_loop()
 		echo "GPU driver Version: $GPU_DRIVER"
 		echo "GPU temp: $GPU_TEMP"
 		echo "GPU fan speed: $GPU_FAN"
-	
+
 		########################################
 		# FPS Stats (vogelperf)
 		########################################
-	
+
 		#echo $APPID
 		if [[ "$APPID_ENABLE" == "False" ]] ; then
 	  		# Do not show text
@@ -406,7 +404,7 @@ funct_main_loop()
 			# Placeholder for now
 	  		echo "Game FPS: 00.00"
 		fi
-	
+
 		########################################
 		# CPU Stats
 		########################################
@@ -422,7 +420,7 @@ funct_main_loop()
 		echo ""
 		echo "CPU Utilization:"
 		echo "$CPU_LOAD"
-	
+
 		########################################
 		# MEMORY Stats
 		########################################
@@ -431,18 +429,18 @@ funct_main_loop()
 		echo "Memory Stats"
 		echo "-----------------------------------------------------------"
 		echo "$MEM_LOAD"
-	
+
 		# let stat's idle for a bit
 		# Removed for now, may not need this
 		# will evailuate if user feeback is given in response to fresh rate
 		# sleep 1s
-	
+
 	done
-	
-	if [ -t 0 ]; then 
-		stty sane; 
+
+	if [ -t 0 ]; then
+		stty sane;
 	fi
-	
+
 	# exit on keypress notification
 	echo -e "\nYou pressed '$keypress' to end the script\n"
 	exit 0

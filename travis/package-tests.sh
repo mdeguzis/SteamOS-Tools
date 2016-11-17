98f8f4446d3b00389155362c0c58cf405138a9cc
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-11-17 13:42:46 -0500

    need to enable repos

--- travis/package-tests.sh
+++ travis/package-tests.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# Travis vars
+BETA_REPO="$BETA_REPO"
+
 # Set applications to test
 APPLICATION_TESTS="kodi retroarch"
 
 # No need to enter repo dir, but do it anyway for now
 cd steamos-tools
+
+# Prep configure repos
+
+# Don't invoke sudo
+sed -i 's/sudo //g' configure-repos.sh
+
+# Remove any sleep commands to speed up process
+sed -i '/sleep/d' configure-repos.sh
+
+# Configures repos
+if [[ "$BETA_REPO" == "true" ]]
+
+	./configure-repos.sh --enable-testing
+
+else
+
+	./configure-repos.sh
+
+fi
 
 for PKG in ${APPLICATION_TESTS};
 do

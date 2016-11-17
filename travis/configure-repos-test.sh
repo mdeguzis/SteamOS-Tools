704396e2df1ef55a073f4838ea016f2ea868ddf9
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-11-17 13:01:34 -0500

    Update configure-repos-test.sh

--- travis/configure-repos-test.sh
+++ travis/configure-repos-test.sh
@@ -6,10 +6,12 @@ cat <<-EOF
 Configure repos test
 ------------------------------------------
 
-Running script from: ${PWD}
-
 EOF
 
+# enter repo folder
+cd steamos-tools
+
+# run tests
 sed -i 's/-q --show-progress -nc/-nc/g' configure-repos.sh
 ./configure-repos.sh && ./configure-repos.sh --remove
 ./configure-repos.sh --enable-testing && ./configure-repos.sh --remove-testing
@@ -17,7 +19,3 @@ sed -i 's/-q --show-progress -nc/-nc/g' configure-repos.sh
 ./configure-repos.sh --default && ./configure-repos.sh --remove
 ./configure-repos.sh --repair && ./configure-repos.sh --remove
 ./configure-repos.sh
-
-ls -la
-
-find / -name "steamos-tools"

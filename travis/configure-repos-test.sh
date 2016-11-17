5671e3b1fdbc020de4d2cdb79e5de9ff455c7e2b
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-11-17 13:06:45 -0500

    Update configure-repos-test.sh

--- travis/configure-repos-test.sh
+++ travis/configure-repos-test.sh
@@ -11,6 +11,12 @@ EOF
 # enter repo folder
 cd steamos-tools
 
+# Don't invoke sudo
+sed -i 's/sudo //g' configure-repos.sh
+
+# Remove any sleep commands to speed up process
+sed -i '/sleep/d' configure-repos.sh
+
 # run tests
 sed -i 's/-q --show-progress -nc/-nc/g' configure-repos.sh
 ./configure-repos.sh && ./configure-repos.sh --remove

e2519ce8120375f38ebe206c2847201e34b5928e
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-11-18 08:28:50 -0500

    add headers

--- travis/configure-repos-test.sh
+++ travis/configure-repos-test.sh
@@ -19,9 +19,53 @@ sed -i '/sleep/d' configure-repos.sh
 
 # run tests
 sed -i 's/-q --show-progress -nc/-nc/g' configure-repos.sh
+
+cat <<-EOF
+
+-----------------------------------------------------
+Test: Default / Remove all configuration
+-----------------------------------------------------
+
+EOF
+
 ./configure-repos.sh && ./configure-repos.sh --remove
+
+cat <<-EOF
+
+-----------------------------------------------------
+Test: Testing Repository / Remove Testing Repository
+-----------------------------------------------------
+
+EOF
+
 ./configure-repos.sh --enable-testing && ./configure-repos.sh --remove-testing
+
+cat <<-EOF
+
+-----------------------------------------------------
+Test: Testing Repository / Remove all configurations 
+-----------------------------------------------------
+
+EOF
+
 ./configure-repos.sh --enable-testing && ./configure-repos.sh --remove
-./configure-repos.sh --default && ./configure-repos.sh --remove
+
+cat <<-EOF
+
+-----------------------------------------------------
+Test: Repair / Remove all configurations
+-----------------------------------------------------
+
+EOF
+
 ./configure-repos.sh --repair && ./configure-repos.sh --remove
+
+cat <<-EOF
+
+-----------------------------------------------------
+Test: Default only
+-----------------------------------------------------
+
+EOF
+
 ./configure-repos.sh

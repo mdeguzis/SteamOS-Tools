0c5e74281e4e2bcd8501ff0a95af8a0d1df769ff
ProfessorKaos64 <mdeguzis@gmail.com>
2017-12-02 19:38:55 -0500

    update supertuxkart backup for data

--- utilities/games/backup-supertuxkart-data.sh
+++ utilities/games/backup-supertuxkart-data.sh
@@ -2,44 +2,53 @@
 
 user="steam"
 date=`date +%Y-%m-%d`
-data="/home/${user}/.config/supertuxkart"
+configs="/home/${user}/.config/supertuxkart"
+data="/home/${user}/.local/share/supertuxkart"
 backup_dir="/home/${user}/backups/supertuxkart/${date}-backup"
 
 sudo mkdir -p ${backup_dir}
 
-if [[ -d "${data}" ]]; then
+backup_items="${configs} ${data}"
 
-	echo -e "\n==> Backing up ${data} to ${backup_dir}\n"
+for item in ${backup_items};
+do
 
-	if ! sudo cp -rv ${data}/* ${backup_dir}; then
-		echo "Failed to make backup!"
-		exit 1
-	fi
+	if [[ -d "${item}" ]]; then
 
-	if ! sudo chown -R ${user}:${user} ${backup_dir}; then
-		echo "Failed to assign permissions!"
-		exit 1
-	fi
+		echo -e "\n==> Backing up ${item} to ${backup_dir}\n"
 
-	echo ""
-	read -erp "Copy backup to alternate directory? (y/n): " response
-
-	if [[ "${response}" == "y" ]]; then
-
-		read -erp "Location: " alt_loc
-		mkdir -p ${alt_loc}
-	
-		if ! cp -r ${backup_dir} ${alt_loc}; then
-
-			echo "Could not copy to alt dir, do you have permissions at the target?"
-
+		if ! sudo cp -r ${item}/* ${backup_dir}; then
+			echo "Failed to make backup!"
+			exit 1
 		fi
+
+		if ! sudo chown -R ${user}:${user} ${backup_dir}; then
+			echo "Failed to assign permissions!"
+			exit 1
+		fi
+
+		echo -e "    Backup complete!"
+
+	else
+
+		echo "No configs directory found at: ${configs}!"
+
 	fi
 
-	echo -e "\n=== Backup complete! ===\n"
+done
 
-else
+echo ""
+read -erp "Copy backup to alternate directory? (y/n): " response
 
-	echo "No save data directory found at: ${data}!"
+if [[ "${response}" == "y" ]]; then
 
+	read -erp "Location: " alt_loc
+	mkdir -p ${alt_loc}
+
+	if ! cp -r ${backup_dir} ${alt_loc}; then
+
+		echo "Could not copy to alt dir, do you have permissions at the target?"
+
+	fi
 fi
+

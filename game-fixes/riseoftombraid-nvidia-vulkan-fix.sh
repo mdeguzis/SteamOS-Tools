826265ddd602c3c3232a7487da414c17e8c79c78
ProfessorKaos64 <mdeguzis@gmail.com>
2018-09-29 10:31:04 -0400

    detect driver to avoid mishaps on non-nvidia

--- game-fixes/riseoftombraid-nvidia-vulkan-fix.sh
+++ game-fixes/riseoftombraid-nvidia-vulkan-fix.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo "For nvidia only! CTRL C to exit now"
 sleep 5s
+echo "Detecting driver nvidia..."
+gpu_driver=$(lshw -c video 2> /dev/null | awk '/configuration/{print $2}' | sed 's/driver=//')
+if [[ ${gpu_driver} != "nvidia" ]]; then
+	echo "ERROR: Failed to locate nvidia driver in use, aborting..."
+	echo "Got: ${gpu_driver}"
+	exit 1
+else
+	echo "Found driver: ${gpu_driver}"
+fi
 sudo apt-get purge amdgpu-pro-vulkan-driver
 sudo apt purge mesa-vulkan-drivers
 sudo apt purge mesa-vulkan-drivers:i386

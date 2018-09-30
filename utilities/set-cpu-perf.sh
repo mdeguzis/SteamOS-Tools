b67c7cfdbb1e8850b72e05bfaeae295ac0f41a30
ProfessorKaos64 <mdeguzis@gmail.com>
2018-09-30 16:06:59 -0400

    fixes for multi core

--- utilities/set-cpu-perf.sh
+++ utilities/set-cpu-perf.sh
@@ -12,26 +12,40 @@ cat<<-EOF
 Note: Your current mode is ${CURR_MODE}
 You will want to ensure you set this back when you are done playing
 if you you forget to type "quit" when done here.
+If reversion to your original mode fails, typically this is 'powersave'.
 
 EOF
 
 echo "Setting CPU to perf mode. enter 'quit' to revert back."
-if ! sudo cpufreq-set -g performance; then
-	echo "ERROR: Failed to set cpufreq mode to performance mode"
-	exit 1
-else
-	echo "Verifying change:"
-	cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-fi
+# Looping code curtosy of reddit user /u/anti_brain_freeze
+for i in `cat /proc/cpuinfo | sed -e '/processor/!d; s/^.*: \([0-9]*\)$/\1/'`
+do
+	if ! sudo cpufreq-set -g performance -c $i; then
+		echo "ERROR: Failed to set cpufreq mode to performance mode"
+		exit 1
+	else
+		echo "Verifying change for cpu$i: $(cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor)"
+	fi
+done
+
+echo "Checking cpufreq-info"
+cpufreq-info -o
 
 # Sit idle
+#CURR_MODE=powersave
 while true; do
 	read input
-	if [[ $input = "quit" ]] || [[ $input = "q" ]] 
+	if [[ $input = "quit" ]] || [[ $input = "q" ]]; then
 		echo "Setting mode back to ${CURR_MODE}"
-		sudo cpufreq-set -g ${CURR_MODE}
-		echo "Verifying mode reversion:"
-		cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-		then break 
+		for i in `cat /proc/cpuinfo | sed -e '/processor/!d; s/^.*: \([0-9]*\)$/\1/'`
+		do
+			if ! sudo cpufreq-set -g ${CURR_MODE} -c $i; then
+				echo "ERROR: Failed to set cpufreq mode to ${CURR_MODE} mode"
+				exit 1
+			else
+				echo "Verifying change for cpu$i: $(cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor)"
+			fi
+		done
 	fi
+	break
 done

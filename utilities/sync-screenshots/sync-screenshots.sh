c1877e6001aa998ee47152c4d9581c61e39eb79f
Michael T. DeGuzis <mdeguzis@gmail.com>
2023-03-13 12:12:43 -0400

    Create symlinks per game/account for google photos sync

--- utilities/sync-screenshots/sync-screenshots.sh
+++ utilities/sync-screenshots/sync-screenshots.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 # Credit: https://foosel.net/til/how-to-automatically-sync-screenshots-from-the-steamdeck-to-google-drive/
 
-action=$1
 main() {
+	if [[ -z ${action} ]]; then
+		echo "[ERROR] Action must be passed as the first arg! One of: install, run"
+		exit 1
+	fi
 	scriptdir=$PWD
 	rclone_ver="1.61.1"
 	REMOTE_NAME='gphoto'
-	REMOTE_DIR='album/Steam screenshots'
+	REMOTE_DIR='album/Steam-screenshots'
 
 	# YAY COMPLEX DIRECTORY NUMBERS
 	# Find current profiles with 'find ~/.steam/steam/ -name "screenshots"'
 	# Also, you can find this in Steam settings
 	# Symlink this so the <service>.path is easy and does not change
-	SCREENSHOTS_DIR="${HOME}/.local/share/Steam/userdata/<STEAM_ID>/760/remote/<STEAM_PROFILE>/screenshots"
 	SOURCE_DIR="${HOME}/.steam_screenshots"
 
-
-	if [[ "${action}" == install ]]; then
+	if [[ "${action}" == "install" ]]; then
 		if ! $(which rclone &>/dev/null); then
 			echo -e "[INFO] Missing rclone, installing..."
 			mkdir -p ~/rclone
@@ -29,8 +30,16 @@ main() {
 			cd ${scriptdir}
 		fi
 
+		# There are screenshot dirs per user and per game, collect them all
 		echo "[INFO] Symlimking screenshots directory for systemd"
-		ln -sfv ${SCREENSHOTS_DIR} ${SOURCE_DIR}
+		mkdir -p ~/.steam_screenshots
+		for d in $(find ${HOME}/.local/share/Steam/userdata/ -name "screenshots");
+		do
+			steam_account=$(echo ${d} | awk -F'/' '{print $8}')
+			game_dir=$(echo ${d} | awk -F'/' '{print $11}')
+			short_name=$(echo $(basename ${d})-${steam_account}-${game_dir})
+			ln -sfv ${d} ${HOME}/.steam_screenshots/${short_name}
+		done
 
 		echo "[INFO] Installing and activating systemd unit files"
 		cp -v sync-screenshots.path ~/.config/systemd/user/
@@ -40,11 +49,12 @@ main() {
 		systemctl --user start sync-screenshots.path
 		systemctl --user status sync-screenshots.path
 
-	elif [[ "${action}" == run ]]; then
+	elif [[ "${action}" == "run" ]]; then
+		# rclone cannot currently sync with "flattened" files (no dirs). Check back on this.
+		# Do not sync useless thumbnails directory
 		echo "[INFO] Syncing screenshots from ${SOURCE_DIR}, please wait..."
-		# Double directory instead of screenshots dir?
 		~/.local/bin/rclone sync -L -P "${SOURCE_DIR}" "${REMOTE_NAME}:${REMOTE_DIR}" \
-			--exclude='/thumbnails/' --exclude='/screenshots/'
+			--exclude=**/thumbnails/**
 	else
 		echo "[ERROR] Unknown action: ${action}. Please use one of: install, run"
 		exit 1
@@ -54,5 +64,6 @@ main() {
 }
 
 # start and log
-main 2>&1 | tee "/tmp/sync-screenshots.log"
+action=$1
+main ${action} 2>&1 | tee "/tmp/sync-screenshots.log"
 echo "[INFO] Log: /tmp/sync-screenshots.log"

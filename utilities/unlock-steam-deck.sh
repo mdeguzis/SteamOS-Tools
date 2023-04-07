c00e5706ac66e2dea3c095cd84d2722e879dd55b
Michael DeGuzis <deguzim@amazon.com>
2023-04-07 10:12:28 -0400

    steam deck unlock script

---
+++ utilities/unlock-steam-deck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Description: A small utility to unlock the read-only filesystem on the Steam Deck 
+#              and install basic gpg keyrings/dev tools for installing software
+
+echo "[INFO] Unlocking filesystem"
+sudo steamos-readonly disable
+
+echo "[INFO] Adding/updating Arch Linux keyrings"
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+
+echo "[INFO] Updating repository index"
+sudo pacman -Syy
+
+echo "[INFO] Installing basic devtools"
+sudo pacman -S --no-confirm base-devel

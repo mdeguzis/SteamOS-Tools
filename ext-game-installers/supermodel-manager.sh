38000cacc2b48a2f7e6070167a81908f593ce18f
Michael T. DeGuzis <mdeguzis@gmail.com>
2024-01-22 20:13:24 -0500

    work on supermodel mgr

--- ext-game-installers/supermodel-manager.sh
+++ ext-game-installers/supermodel-manager.sh
@@ -13,6 +13,7 @@ function show_help() {
 	cat<<-HELP_EOF
 	--help|-h			Show this help page
 	--install			Install supermodel
+	--configure-input|-c		Pre-configure inputs (Thank you https://github.com/WarpedPolygon!)
 	--add-game			Add game desktop file for Steam
 	--remote-game			Remove game desktop file for Steam
 	--game-name			Set name of game to add/remove
@@ -26,7 +27,45 @@ function install_supermodel() {
 	# use the Flatpak (much safer install
 	# https://gitlab.com/es-de/emulationstation-de/-/blob/master/USERGUIDE.md#arcade-and-neo-geo
 	# https://flathub.org/apps/com.supermodel3.Supermodel
-	flatpak install --user com.supermodel3.Supermodel -y
+	OS_TYPE=$1
+	
+	# Support ChimeraOS/Steam Deck
+	if [[ "${OS_TYPE}" == "ChimeraOS" ]]; then
+		# ChimeraOS
+		# For some reason, the flatpak doesn't like to run on ChimeraOS (yet)
+		# Error: OpenGL initialization failed: unknown error
+		INSTALL_TYPE="ChimeraOS"
+		sudo frzr-unlock
+
+		echo -e "\n[INFO] Initializing and refreshing keys, please wait..."
+		sudo pacman -Syy
+		sudo pacman -S archlinux-keyring --noconfirm
+		sudo pacman-key --init
+		sudo pacman-key --populate archlinux
+
+		# If this fails, regenerate mirrors with the curl command and upgrade keyring
+		# 	https://wiki.archlinux.org/title/mirrors
+		# 	sudo pacman -Sy archlinux-keyring --noconfirm
+		# This also manifests as getting install errors such as:
+		# 	error: pkgconf: signature from "Johannes Löthberg <johannes@kyriasis.com>" is marginal trust
+		sudo pacman -Sy sdl2 sdl2_net devtools base-devel --noconfirm
+
+		# Clone
+		echo -e "\n[INFO] Building and installing supermodel"
+		mkdir -p ${HOME}/src
+		if [[ ! -d "${HOME}/src/supermodel" ]]; then
+			git clone https://github.com/trzy/Supermodel ~/src/supermodel
+		else
+			git -C "${HOME}/src/supermodel" pull
+		fi
+		cd "${HOME}/src/supermodel"
+		make -f Makefiles/Makefile.UNIX NET_BOARD=1
+
+		# When built, we need to execute this link from the src root
+		sudo ln -sfv $(readlink -f bin/supermodel) /usr/bin/supermodel
+	else
+		flatpak install --user com.supermodel3.Supermodel -y
+	fi
 
 	echo -e "\nBasic usage: https://www.supermodel3.com/Usage.html"
 }
@@ -46,6 +85,10 @@ main() {
 
 			--add-game|-a)
 				ADD_GAME="true"
+				;;
+
+			--configure-input|-p)
+				CONFIGURE_INPUT="true"
 				;;
 
 			--game-name|-n)
@@ -96,9 +139,15 @@ main() {
 		shift
 	done
 
-	# Do we need to buidl
+	if [[ -f "/usr/bin/frzr-unlock" ]]; then
+		OS_TYPE="ChimeraOS"
+	else
+		OS_TYPE="SteamDeck"
+	fi
+	echo -e "[INFO] Install type: ${OS_TYPE}"
+
 	if [[ ${INSTALL} == "true" ]]; then
-		install_supermodel
+		install_supermodel ${OS_TYPE}
 	fi	
 
 	# Add desktop files for games
@@ -130,6 +179,39 @@ main() {
 		sed -i "s|GAME_NAME|${GAME_NAME}|g" "${HOME}/.local/share/applications/supermodel-${GAME_BASENAME}.desktop"
 		sed -i "s|GAME_ZIP|${GAME_ZIP}|g" "${HOME}/.local/share/applications/supermodel-${GAME_BASENAME}.desktop"
 		sed -i "s|DEVICE_RES|${DEVICE_RES}|g" "${HOME}/.local/share/applications/supermodel-${GAME_BASENAME}.desktop"
+		sed -i "s|START_PATH|${HOME}/src/supermodel|g" "${HOME}/.local/share/applications/supermodel-${GAME_BASENAME}.desktop"
+		if [[ "${OS_TYPE}" == "ChimeraOS" ]]; then
+			echo "[INFO] Using built binary"
+			sed -i "s|SUPERMODEL_BIN|/usr/bin/supermodel|g" "${HOME}/.local/share/applications/supermodel-${GAME_BASENAME}.desktop"
+		else
+			echo "[INFO] Using flatpak"
+			sed -i "s|SUPERMODEL_BIN|/usr/bin/flatpak run com.supermodel3.Supermodel|g" "${HOME}/.local/share/applications/supermodel-${GAME_BASENAME}.desktop"
+		fi
+	fi
+
+	# Configure input?
+	if [[ ${CONFIGURE_INPUT} == "true" ]]; then
+		cat<<-EOF
+
+		Configure input:
+
+		1) xinput
+		2) dinput
+
+		EOF
+		read -erp "Choice: " INPUT_CHOICE
+		if [[ ${INPUT_CHOICE} == "1" ]]; then
+			cp -v "${GIT_ROOT}/cfgs/supermodel3/xinput/Supermodel.ini" "${HOME}/src/supermodel/Config/"
+		elif [[ ${INPUT_CHOICE} == "2" ]]; then
+			cp -v "${GIT_ROOT}/cfgs/supermodel3/dinput/Supermodel.ini" "${HOME}/src/supermodel/Config/"
+		else
+			echo "[ERROR] Invalid choice!"
+			exit 1
+		fi
+
+		mkdir -p "${HOME}/src/supermodel/NVRAM"
+		cp -r "${GIT_ROOT}/cfgs/supermodel3/nvram/" "${HOME}/src/supermodel/NVRAM/"
+
 	fi
 
 }
@@ -137,8 +219,7 @@ main() {
 # Start and log
 main "$@" 2>&1 | tee "${LOG_FILE}"
 echo "[INFO] Log: ${LOG_FILE}"
-echo "[INFO] Looking for notes on how to pre-configure controls for your gamepad?"
-ehco "[INFO] https://forums.launchbox-app.com/files/file/3857-sega-model-3-supermodel-git-everything-pre-configured-inc-controls-for-pc-controller-mouse-light-guns-test-menus-configured-free-play-all-games-in-english-2-player-mouse-support-audio-adjusted-layout-imagesthe-whole-9-yards/"
+echo "[INFO] Looking for notes on how to pre-configure controls for your gamepad? See cfgs/supermodel3/README.md"
 
 # Trim logs
 find /tmp -name "supermodel-mgr-*" -mtime 14 -exec -delete \; 2>/dev/null

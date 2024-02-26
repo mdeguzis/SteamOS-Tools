77b7a6e66b6fff5bfde3a5db31dd6a80333fa932
Michael T. DeGuzis <mdeguzis@gmail.com>
2024-02-26 19:39:09 +0000

    Add exes

--- utilities/update-emulators.sh
+++ utilities/update-emulators.sh
@@ -7,13 +7,30 @@
 
 update_emu_flatpak ()
 {
-    name=$1;
-    ID=$2;
-    echo "[INFO] Updating $name";
-    flatpak update $ID -y;
-    flatpak override $ID --filesystem=host --user;
-    flatpak override $ID --share=network --user;
+	name=$1;
+	ID=$2;
+	echo "[INFO] Updating $name";
+	flatpak update $ID -y;
+	flatpak override $ID --filesystem=host --user;
+	flatpak override $ID --share=network --user;
 }
+
+update_windows_exe ()
+{
+	name=$1;
+	URL=$2;
+	echo "[INFO] Updating $name";
+
+	# Git repo releases page? get latest
+	# The ~/Applicaitons dir is compliant with ES-DE
+	if echo "${URL}" | grep -q "zip"; then
+		echo "[INFO] Downloading and extracting ZIP for ${name}"
+		curl -sLo "/tmp/${name}.zip" "${URL}"
+		unzip -o "/tmp/${name}.zip" -d "${HOME}/Applications/${name}"
+	fi
+
+}
+
 
 echo -e "[INFO] Updating emulators (Flatpaks)\n"
 sleep 3
@@ -28,4 +45,9 @@ update_emu_flatpak "Xemu-Emu" "app.xemu.xemu"
 update_emu_flatpak "ScummVM" "org.scummvm.ScummVM"
 update_emu_flatpak "melonDS" "net.kuribo64.melonDS"
 update_emu_flatpak "RMG" "com.github.Rosalie241.RMG"
+
+echo -e "\n[INFO] Updating Windows EXE's (e.g. xenia))"
+update_windows_exe "xenia" "https://github.com/xenia-canary/xenia-canary/releases/download/experimental/xenia_canary.zip"
+update_windows_exe "xenia" "https://github.com/xenia-project/release-builds-windows/releases/latest/download/xenia_master.zip"
+
 

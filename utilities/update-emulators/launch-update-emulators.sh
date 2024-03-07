20bdfb955cb36871e98c8974b81971b71496ca48
Michael DeGuzis <mdeguzis@gmail.com>
2024-03-06 21:01:22 -0500

    show dialog for script update

--- utilities/update-emulators/launch-update-emulators.sh
+++ utilities/update-emulators/launch-update-emulators.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Spaws a new Konsole window to run the script
+# Spawns a new terminal window to run the script
 
 # Update git source if possible first
 mkdir -p "${HOME}/.local/bin"
-curl -o "${HOME}/.local/bin/update-emulators.sh.new" "https://raw.githubusercontent.com/mdeguzis/SteamOS-Tools/master/utilities/update-emulators/update-emulators.sh"
+curl -o "${HOME}/.local/bin/update-emulators.sh.new" "https://raw.githubusercontent.com/mdeguzis/SteamOS-Tools/master/utilities/update-emulators/update-emulators.sh"; sleep 3 | zenity --progress --auto-close --pulsate --text="Fetching the latest update-emulators.sh script" --title="Updater" --width=600 --height=250 2>/dev/null
 
 if [[ $? -eq 0 ]]; then
 	mv "${HOME}/.local/bin/update-emulators.sh.new" "${HOME}/.local/bin/update-emulators.sh"

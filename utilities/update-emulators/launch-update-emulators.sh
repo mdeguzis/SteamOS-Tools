f5ddf502d4e6ea6a9c9845303a406ed89647350c
Michael DeGuzis <mdeguzis@gmail.com>
2024-03-07 14:45:51 -0500

    Note when failures

--- utilities/update-emulators/launch-update-emulators.sh
+++ utilities/update-emulators/launch-update-emulators.sh
@@ -3,11 +3,12 @@
 
 # Update git source if possible first
 mkdir -p "${HOME}/.local/bin"
-curl -o "${HOME}/.local/bin/update-emulators.sh.new" "https://raw.githubusercontent.com/mdeguzis/SteamOS-Tools/master/utilities/update-emulators/update-emulators.sh"; sleep 3 | zenity --progress --auto-close --pulsate --text="Fetching the latest update-emulators.sh script" --title="Updater" --width=600 --height=250 2>/dev/null
+curl -o "${HOME}/.local/bin/update-emulators.sh.new" "https://raw.githubusercontent.com/mdeguzis/SteamOS-Tools/master/utilities/update-emulators/update-emulators.sh" && sleep 3 | zenity --progress --auto-close --pulsate --text="Fetching the latest update-emulators.sh script" --title="Updater" --width=600 --height=250 2>/dev/null
 
 if [[ $? -eq 0 ]]; then
 	mv "${HOME}/.local/bin/update-emulators.sh.new" "${HOME}/.local/bin/update-emulators.sh"
 else
+	zenity --info --text="Failed to install new script version, reusing existing script."
 	rm -f "${HOME}/.local/bin/update-emulators.sh.new"
 fi
 chmod +x "${HOME}/.local/bin/update-emulators.sh"

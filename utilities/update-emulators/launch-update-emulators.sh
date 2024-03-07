c8a93449ff66d9044277eb7417480acabdefba96
Michael DeGuzis <mdeguzis@gmail.com>
2024-03-06 20:46:25 -0500

    Auto update script

--- utilities/update-emulators/launch-update-emulators.sh
+++ utilities/update-emulators/launch-update-emulators.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # Spaws a new Konsole window to run the script
 
+# Update git source if possible first
+mkdir -p "${HOME}/.local/bin"
+curl -o "${HOME}/.local/bin/update-emulators.sh.new" "https://raw.githubusercontent.com/mdeguzis/SteamOS-Tools/master/utilities/update-emulators/update-emulators.sh"
+
+if [[ $? -eq 0 ]]; then
+	mv "${HOME}/.local/bin/update-emulators.sh.new" "${HOME}/.local/bin/update-emulators.sh"
+else
+	rm -f "${HOME}/.local/bin/update-emulators.sh.new"
+fi
+
 # Ignore dumb .so warnings by setting LD_PRELOAD to undefined
 export LD_PRELOAD=""
 konsole -e '$SHELL -c "${HOME}/.local/bin/update-emulators.sh && exit; $SHELL"'

4ea3c502cf6d145d6e9c4050c9fc529a2e885c0a
Michael DeGuzis <mdeguzis@gmail.com>
2024-06-10 20:06:26 -0400

    Spacing

--- utilities/steam-deck/run-decky-cloud-save.sh
+++ utilities/steam-deck/run-decky-cloud-save.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-~/homebrew/plugins/decky-cloud-save/rclone copy --filter-from ~/homebrew/settings/decky-cloud-save/sync_paths_filter.txt / backend:decky-cloud-save --copy-links --verbose --verbose
+hostname=$(cat /etc/hostname)
+~/homebrew/plugins/decky-cloud-save/rclone copy \
+	--filter-from ~/homebrew/settings/decky-cloud-save/sync_paths_filter.txt \
+	/ backend:decky-cloud-save-${hostname} \
+	--copy-links --verbose --verbose
